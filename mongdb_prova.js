53fbdb090150f5e6cff6c696654d96c381ddcd64
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2018-08-20 16:46:21 +0200

    edit

--- mongdb_prova.js
+++ mongdb_prova.js
@@ -1,36 +1,8 @@
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017";
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
 
-var count = 0;
-
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db('mydb');
-  /*
-  dbo.createCollection('customers', function(err, res) {
-  	if (err) throw err;
-  	console.log("Collection created!");
-  	db.close();	
-  });*/
-/*
-  var myobj = [
-  	{ name: 'Chocolate Heaven'},
-    { name: 'Tasty Lemon'},
-    { name: 'Vanilla Dream'}
-   ];
-
-   dbo.collection('products').insertMany(myobj, function(err, res) {
-   	if (err) throw err;
-   	db.close();
-   });
-*/
-   dbo.collection("customers").findOne({}, function(err, result) {
-    if (err) throw err;
-    console.log(result.address);
-    db.close();
-   });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Connected!');
 });
-
-function foo() {
-	console.log('Th coount is: ' + count);
-}
