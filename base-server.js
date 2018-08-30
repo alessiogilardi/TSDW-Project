37cc8fd045927115a663297deb265668dddc13f7
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2018-08-30 17:01:26 +0200

    edit

--- base-server.js
+++ base-server.js
@@ -5,8 +5,8 @@ var http  = require('http');
 
 var port    = process.env.PORT || 8443;
 var options = {
-    key: fs.readFileSync( 'encryption/server.key' ),
-    cert: fs.readFileSync( 'encryption/server.pem' ),
+    key: fs.readFileSync( 'encryption/PRIVATE.key' ),
+    cert: fs.readFileSync( 'encryption/PUBLIC.pem' ),
     requestCert: false,
     rejectUnauthorized: false
 };
