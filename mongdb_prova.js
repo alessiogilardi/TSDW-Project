56f640dc8b44379898e0e80a442faffaa00c48cd
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2018-09-06 21:59:23 +0200

    Pulizia codice e utilizzo nuove funzionalità

--- mongdb_prova.js
+++ mongdb_prova.js
@@ -1,19 +1,42 @@
-var mongoose = require('mongoose');
-var schemas = require('./schemas.js');
+//const mongoose  = require('mongoose');
+//const schemas   = require('./schemas.js');
+const models    = require('./models.js');
+const db        = require('./db-connect.js');
 
+var mongoose = db.connect();
 
+var mOperator = new models.AirOperator({
+        _id: new mongoose.Types.ObjectId(),
+        name: "mName",
+        location: {
+          country: "mCountry",
+          city: "mCity",
+          address: "mAddress"
+        }
+    });
+
+mOperator.save((err) => {
+  if (err)
+    return console.log(err);
+});
+
+db.disconnect();
+
+/*
 mongoose.connect('mongodb://localhost:27017/TSDW', {useNewUrlParser: true});
 
 
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on('error', console.error.bind(console, 'Connection error:'));
 db.once('open', () => {
   console.log('Connected!');
-  //schemas.newOperatore('aaa', 'bbb', 'ccc', 'ddd');
 });
+*/
 
-schemas.newOperatore('2222', '2222bbb', '2222ccc', '2222ddd');
+// schemas.newOperatore('2222', '2222bbb', '2222ccc', '2222ddd');
 
+/*
 setTimeout(() => {
   mongoose.disconnect();
 }, 1500);
+*/
