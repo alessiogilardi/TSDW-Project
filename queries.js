72dc43ed98907301d36f7f59503953854786aa9a
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2018-09-07 14:44:34 +0200

    Ristrutturazione e pulizia codice

--- queries.js
+++ queries.js
@@ -1,36 +1,48 @@
 const mongoose = require('mongoose');
 const models = require('./models.js');
 
-exports.createAirOperator = (oName, oCountry, oCity, oAddress, oAM = null, oCQM = null, oSM = null, oBases = []) => {
-  new models.AirOperator({
-    _id: new mongoose.Types.ObjectId(),
-    name: oName,
-    location: {
-      country: oCountry,
-      city: oCity,
-      address: oAddress
-    },
-    roles: {
-      AM: oAM,
-      CQM: oCQM,
-      SM: oSM
-    },
-    bases: oBases
-  }).save((err) => {
-      if (err)
-        return console.log(err);
-  });
+exports.AirOperator = {
+
+  create: (oName, oCountry, oCity, oAddress, oAM = undefined, oCQM = undefined, oSM = undefined, oBases = undefined) => {
+    new models.AirOperator({
+      _id: new mongoose.Types.ObjectId(),
+      name: oName,
+      location: {
+        country: oCountry,
+        city: oCity,
+        address: oAddress
+      },
+      roles: {
+        AM: oAM,
+        CQM: oCQM,
+        SM: oSM
+      },
+      bases: oBases
+    }).save((err) => {
+        if (err)
+          return console.log(err);
+    });
+  },
+
+  updateByName: (oName, newValues) => {
+    models.AirOperator.updateOne({name: oName}, newValues, () => {});
+  }
+};
+
+exports.Base = {
+  create: () => {
+
+  },
+  updateByName: () => {
+
+  }
 }
 
-exports.updateAirOperatorByName = (oName, newValues) => {
-  
-  models.AirOperator.findOneAndUpdate({name: "oName"}, {name: "aaa"}, (err) => {
-    if(err) console.log(err);
-  });
-  
-  /*
-  models.AirOperator.update({_id: mongoose.Types.ObjectId('5b92416ba3d7620a1422ef03')}, { $set: JSON.parse('{"location": {"city": "Genoa"}}')}, (err, user) => {
-    if(err) console.log(err);
-  });
-  */
+exports.Personnel = {
+  create: () => {
+
+  },
+  updateByName: () => {
+
+  }
 }
