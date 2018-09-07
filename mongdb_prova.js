6053d00924a711e2eb6ffd70727fba3e0924332c
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2018-09-07 12:30:05 +0200

    Varie

--- mongdb_prova.js
+++ mongdb_prova.js
@@ -2,41 +2,16 @@
 //const schemas   = require('./schemas.js');
 const models    = require('./models.js');
 const db        = require('./db-connect.js');
+const queries   = require('./queries.js');
 
 var mongoose = db.connect();
 
-var mOperator = new models.AirOperator({
-        _id: new mongoose.Types.ObjectId(),
-        name: "mName",
-        location: {
-          country: "mCountry",
-          city: "mCity",
-          address: "mAddress"
-        }
-    });
+//queries.createAirOperator("oName", "oCountry", "oCity", "oAddress");
 
-mOperator.save((err) => {
-  if (err)
-    return console.log(err);
-});
+
+
+var edited = {country: 'Australia', city: 'Genoa'};
+
+queries.updateAirOperatorByName('oName', edited);
 
 db.disconnect();
-
-/*
-mongoose.connect('mongodb://localhost:27017/TSDW', {useNewUrlParser: true});
-
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Connection error:'));
-db.once('open', () => {
-  console.log('Connected!');
-});
-*/
-
-// schemas.newOperatore('2222', '2222bbb', '2222ccc', '2222ddd');
-
-/*
-setTimeout(() => {
-  mongoose.disconnect();
-}, 1500);
-*/
