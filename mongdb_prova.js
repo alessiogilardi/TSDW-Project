6f15ed0b3a93120cfb538c3c217c451833f69196
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2018-09-07 14:44:05 +0200

    Fatte prove di insert e update

--- mongdb_prova.js
+++ mongdb_prova.js
@@ -1,17 +1,16 @@
-//const mongoose  = require('mongoose');
-//const schemas   = require('./schemas.js');
 const models    = require('./models.js');
 const db        = require('./db-connect.js');
 const queries   = require('./queries.js');
 
 var mongoose = db.connect();
 
-//queries.createAirOperator("oName", "oCountry", "oCity", "oAddress");
+//queries.AirOperator.create('qwe', 'sss', 'sss', 'sss');
 
 
 
-var edited = {country: 'Australia', city: 'Genoa'};
+var edited = {'location.city': 'sfsdsdfds'};
 
-queries.updateAirOperatorByName('oName', edited);
+queries.AirOperator.updateByName('qwe', edited);
+
 
 db.disconnect();
