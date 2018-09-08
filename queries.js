c67245c5699e32ff3111750c9b544502fd20cfca
Alessio Bollea <alessio.bollea@live.it>
2018-09-08 17:45:30 +0200

    Scrittura query per schema delle basi
    Inserimento, aggiornamento per nome.
    Aggiunta della base appena creata alla lista di basi dell'operatore aereo corrispondente.

--- queries.js
+++ queries.js
@@ -4,7 +4,7 @@ const models = require('./models.js');
 /* Rinominare le funzioni create in insert */
 exports.AirOperator = {
 
-  create: (oName, oCountry, oCity, oAddress, oAM = undefined, oCQM = undefined, oSM = undefined, oBases = undefined) => {
+  create: (oName, oCountry, oCity, oAddress, oAM = undefined, oCQM = undefined, oSM = undefined, oBases = []) => {
     new models.AirOperator({
       _id: new mongoose.Types.ObjectId(),
       name: oName,
@@ -44,7 +44,6 @@ exports.AirOperator = {
       if (err)
         return console.log(err);
     });
-
   },
 
   updateByName: (oName, newValues) => {
@@ -56,19 +55,55 @@ exports.AirOperator = {
 };
 
 exports.Base = {
-  create: () => {
-
+	
+  create: (oName, oAirOperator, oCountry, oCity, oLatitude = undefined, oLongitude = undefined, oViceAM = undefined, oBaseSupervisor = undefined, oPilots = undefined, \
+		   oEquip = undefined, oMain, oMainteiners = undefined, oDrones = []) => {
+	new models.Base({
+		_id: new mongoose.Types.ObjectId(),
+		name: oName,
+		airOperator: oAirOperator,
+		location: {
+			country: oCountry,
+			city: oCity,
+			address: oAddress,
+			latitude: oLatitude,
+			longitude: oLatitude,
+		},
+		roles: {
+			ViceAM: oViceAM,
+			BaseSupervisor: oBaseSupervisor
+		},
+		staff: {
+			pilots: oPilots,
+			equip: oEquip,
+			mainteiners: oMainteiners
+		},
+		drones: oDrones
+	}).save((err, results) => {
+		// results contiene il documento json della base appena creata
+		if (err)
+          return console.log(err);
+		// Quando la query viene eseguita, devo aggiungere l'id della base appena creata alla lista di basi dell'operatore aereo corrispondente
+		var edited = {$push: {'bases': results._id}};
+		AirOperator.updateByName(results.airOperator, edited);
+	});
   },
+  
   updateByName: () => {
-
+	models.Base.updateOne({name: oName}, newValues, (err) => {
+		if (err)
+			console.log(err);
+    });
   }
-}
+};
 
 exports.Personnel = {
+	
   create: () => {
 
   },
+  
   updateByName: () => {
 
   }
-}
+};
