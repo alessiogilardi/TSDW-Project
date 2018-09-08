f2acb7a0abd20fc43458416cf3adcc4cf44465db
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2018-09-08 10:55:28 +0200

    Inserita funzione AirOperator.create2

--- queries.js
+++ queries.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const models = require('./models.js');
 
+/* Rinominare le funzioni create in insert */
 exports.AirOperator = {
 
   create: (oName, oCountry, oCity, oAddress, oAM = undefined, oCQM = undefined, oSM = undefined, oBases = undefined) => {
@@ -24,8 +25,33 @@ exports.AirOperator = {
     });
   },
 
+  create2: (airOperator) => {
+    new models.AirOperator({
+      _id: new mongoose.Types.ObjectId(),
+      name: airOperator.name,
+      location: {
+        country: airOperator.location.country,
+        city: airOperator.location.city,
+        address: airOperator.location.address
+      },
+      roles: {
+        AM: airOperator.roles.AM,
+        CQM: airOperator.roles.CQM,
+        SM: airOperator.roles.SM
+      },
+      bases: airOperator.bases
+    }).save((err) => {
+      if (err)
+        return console.log(err);
+    });
+
+  },
+
   updateByName: (oName, newValues) => {
-    models.AirOperator.updateOne({name: oName}, newValues, () => {});
+    models.AirOperator.updateOne({name: oName}, newValues, (err) => {
+      if (err)
+        console.log(err);
+    });
   }
 };
 
