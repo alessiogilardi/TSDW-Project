33d0b2dba533d59f266b0915bad3d1a756e3d328
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2018-09-11 09:11:19 +0200

    Inseriti rifermenti tra modelli (foreign keys) e inserite annotazioni su possibili modifiche

--- schemas.js
+++ schemas.js
@@ -15,8 +15,8 @@ exports.airOperatorSchema = new mongoose.Schema({
   },
   roles: {
     AM: {type: ObjectId, default: null, ref: 'personnel'},
-    CQM: {type: ObjectId, default: null},
-    SM: {type: ObjectId, default: null}
+    CQM: {type: ObjectId, default: null, ref: 'personnel'},
+    SM: {type: ObjectId, default: null, ref: 'personnel'}
   },
   bases: [{type: ObjectId, default: [], ref: 'base'}]
 });
@@ -32,7 +32,7 @@ exports.personnelSchema = new mongoose.Schema({
         city: String,
         address: String
     },
-    airOperator: ObjectId,
+    airOperator: {type: ObjectId, ref: 'air_operator'},
     base: {type: ObjectId, ref: 'base'},
     roles: [String],
     missions: [{type: ObjectId, default: [], ref: 'mission'}],
@@ -60,29 +60,29 @@ exports.basesSchema = new mongoose.Schema({
         longitude: Number
     },
     roles: {
-        ViceAM: {type: ObjectId, default: null},
-        BaseSupervisor: {type: ObjectId, default: null}
+        ViceAM: {type: ObjectId, default: null, ref: 'personnel'},
+        BaseSupervisor: {type: ObjectId, default: null, ref: 'personnel'}
     },
     staff: {
-        pilots: [{type: ObjectId, default: []}],
-        equip: [{type: ObjectId, default: []}],
-        mainteiners: [{type: ObjectId, default: []}]
+        pilots: [{type: ObjectId, default: [], ref: 'personnel'}],
+        equip: [{type: ObjectId, default: [], ref: 'personnel'}],
+        mainteiners: [{type: ObjectId, default: [], ref: 'personnel'}]
     },
-    drones: [{type: ObjectId, default: []}]
+    drones: [{type: ObjectId, default: [], ref: 'drone'}]
 });
 
 exports.dronesSchema = new mongoose.Schema({
     _id: ObjectId,
     number: {type: String, unique: true}, /* Non sapendo se sia numerico o alfanumerico */
     type: String,
-    operator: ObjectId,
-    base: ObjectId,
+    airOperator: {type: ObjectId, ref: 'air_operator'},
+    base: {type: ObjectId, ref: 'base'},
     state: {
         generalState: String,
         lastMaintenance: Date,
         notes: String
     },
-    missions: [{type: ObjectId, default: []}]
+    missions: [{type: ObjectId, default: [], ref: 'mission'}]
 });
 
 exports.missionsSchema = new mongoose.Schema({
@@ -93,36 +93,60 @@ exports.missionsSchema = new mongoose.Schema({
         longitude: Number
     },
     type: String,
-    base: ObjectId,
-    supervisor: ObjectId,
+    base: {type: ObjectId, ref: 'base'},
+    supervisor: {type: ObjectId, ref: 'personnel'},
     duration: {
         expectedDuration: Number,
         effectiveDuration: Number
     },
     description: String,
     flightPlan: String,
-    drones: [ObjectId],
-    pilots: [ObjectId],
-    equip: [ObjectId],
-    mainteiners: [{type: ObjectId, default: []}],
-    logbooks: [{type: ObjectId, default: []}],
-    qtb: [{type: ObjectId, default: []}]
+    drones: [{type: ObjectId, ref: 'drone'},],
+    pilots: [{type: ObjectId, ref: 'personnel'},],
+    equip: [{type: ObjectId, ref: 'personnel'},],
+    mainteiners: [{type: ObjectId, default: [], ref: 'personnel'}],
+    logbooks: [{type: ObjectId, default: [], ref: 'logbook'}],
+    qtb: [{type: ObjectId, default: [], ref: 'qtb'}]
 });
 
+/*
+    Il tempo di volo ptrebbe essere composto da voli separati?
+    Nel caso io lo strutturerei in questo modo:
+    info: {
+        fligths: [{
+            flightStart: Date,
+            flightEnd: Date,
+            notes: String
+        }],
+        totalFlightTime: Number  // Non so se serva mantenerlo o sia meglio desumerlo dai campi sopra
+    }
+*/
 exports.logbooksSchema = new mongoose.Schema({
     _id: ObjectId,
-    pilotWriter: ObjectId,
-    mission: ObjectId,
+    pilotWriter: {type: ObjectId, ref: 'personnel'}
+    mission: {type: ObjectId, ref: 'mission'}
     info: {
         flightTime: Number,
         notes: String
     }
 });
 
+/*
+    Stesso discorso, come sopra, anzi forse qui è più importante 
+    dato che sono dati sicuramente più precisi presi dal drone stesso
+    info: {
+        fligths: [{
+            flightStart: Date,
+            flightEnd: Date,
+            notes: String
+        }],
+        totalFlightTime: Number  // Non so se serva mantenerlo o sia meglio desumerlo dai campi sopra
+    }
+*/
 exports.qtbSchema = new mongoose.Schema({
     _id: ObjectId,
-    drone: ObjectId,
-    mission: ObjectId,
+    drone:  {type: ObjectId, ref: 'drone'},
+    mission:  {type: ObjectId, ref: 'mission'},
     info: {
         fligthTime: Number,
         notes: String
