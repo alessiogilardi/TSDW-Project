1ffe1205f8e1302ee2eb3821e847c95023982214
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2018-09-17 22:18:55 +0200

    Prove varie

--- mongodb_prova.js
+++ mongodb_prova.js
@@ -12,7 +12,7 @@ var mongoose = db.connect();
 
 //queries.AirOperator.updateByName('qwe', edited);
 
-//queries.Base.insert("Base 4", 'eurodrone' , "Italy", "La Speza", "22", "22");
+queries.Base.insert2("Base 4", 'eurodrone' , "Italy", "La Speza", "22", "22");
 /*
 queries.AirOperator.prova('eurodrone', (doc) => {
 	console.log(doc);
@@ -31,6 +31,5 @@ models.Base.findOne()
 */
 
 //queries.Personnel.insert2('42', 'Alessio', 'Bollea', 'ilmiocf', 'Italy', 'Genoa', 'nonvelodico', 'eurodrone', 'Base 4', ['AM', 'BaseSupervisor', 'pilot']);
-queries.Drone.insert('1lm10dr0n3', 'Heavy', 'eurodrone', 'Base 4', 'Ready');
+//queries.Drone.insert('1lm10dr0n3', 'Heavy', 'eurodrone', 'Base 4', 'Ready');
 
-db.disconnect();
