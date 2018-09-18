91b8554f9252a272720803003f6ab0df47c67d71
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2018-09-18 19:02:23 +0200

    Pulizia codice

--- mongodb_prova.js
+++ mongodb_prova.js
@@ -6,29 +6,7 @@ var mongoose = db.connect();
 
 //queries.AirOperator.insert('eurodrone', 'Italy', 'Genoa', 'Corso Europa 22');
 
-
-
-//var edited = {'location.city': 'sfsdsdfds'};
-
-//queries.AirOperator.updateByName('qwe', edited);
-
 queries.Base.insert2("Base 4", 'eurodrone' , "Italy", "La Speza", "22", "22");
-/*
-queries.AirOperator.prova('eurodrone', (doc) => {
-	console.log(doc);
-});
-*/
-
-//console.log(queries.AirOperator.findByName('eurodrone'));
-
-
-/*
-models.Base.findOne()
-.populate('airOperator')
-.exec((err, res) => {
-	console.log(res);
-});
-*/
 
 //queries.Personnel.insert2('42', 'Alessio', 'Bollea', 'ilmiocf', 'Italy', 'Genoa', 'nonvelodico', 'eurodrone', 'Base 4', ['AM', 'BaseSupervisor', 'pilot']);
 //queries.Drone.insert('1lm10dr0n3', 'Heavy', 'eurodrone', 'Base 4', 'Ready');
