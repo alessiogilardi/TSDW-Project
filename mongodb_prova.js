924bffe6edb55a90b1030edb92f58377f8632de7
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2018-09-19 22:50:02 +0200

    Prove varie

--- mongodb_prova.js
+++ mongodb_prova.js
@@ -4,10 +4,11 @@ const queries   = require('./queries.js');
 
 var mongoose = db.connect();
 
-queries.AirOperator.insert('eurodrone', 'Italy', 'Genoa', 'Corso Europa 22');
+//queries.AirOperator.insert('eurodrone', 'Italy', 'Genoa', 'Corso Europa 22');
 
 //queries.Base.insert2("Base 4", 'eurodrone' , "Italy", "La Speza", "22", "22");
 
-//queries.Personnel.insert2('42', 'Alessio', 'Bollea', 'ilmiocf', 'Italy', 'Genoa', 'nonvelodico', 'eurodrone', 'Base 4', ['AM', 'BaseSupervisor', 'pilot']);
+queries.Personnel.insert('42', 'Alessio', 'Bollea', 'ilmiocf', 'Italy', 'Genoa', 'nonvelodico', 'eurodrone', 'Base 4', ['pilot','maintainer'], 'AM', ['ViceAM'], false, 'licId123', 'SuperLicenseType', 5, new Date(), 'LM');
+//aIdTelegram, aName, aSurname, aCf, aCountry, aCity, aAddress, aAirOperatorName, aBaseName, aOccupation, aAirOperatorRole = undefined, aBaseRole = undefined, aLocPermission = false, aLicenseId = undefined, aLicenseType = undefined, aLicenseMaxMissionRank = undefined, aLicenseExpireDate = undefined, aDroneTypes = undefined
 //queries.Drone.insert('1lm10dr0n3', 'Heavy', 'eurodrone', 'Base 4', 'Ready');
 
