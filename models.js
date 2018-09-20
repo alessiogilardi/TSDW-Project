53a36e9892558ea89335548c0552ec6f9dc6d5a8
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2018-09-20 21:51:08 +0200

    Aggiunto modello Battery

--- models.js
+++ models.js
@@ -8,6 +8,7 @@ exports.AirOperator = mongoose.model('air_operator', schemas.airOperatorSchema);
 exports.Personnel   = mongoose.model('personnel', schemas.personnelSchema);
 exports.Base        = mongoose.model('base', schemas.basesSchema);
 exports.Drone       = mongoose.model('drone', schemas.dronesSchema);
+exports.Battery     = mongoose.model('battery', schemas.batterySchema);
 exports.Mission     = mongoose.model('mission', schemas.missionsSchema);
 exports.Logbook     = mongoose.model('logbook', schemas.logbooksSchema);
 exports.Qtb         = mongoose.model('qtb', schemas.qtbSchema);
