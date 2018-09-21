5de7f03f8651eb8dd8e331e7fabaf743ac026e24
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2018-09-21 16:09:32 +0200

    Inseriti alcuni vincoli di integrità
    Da decidere se gestire così i vincoli o come fatto finora

--- db-integrity-constraints.js
+++ db-integrity-constraints.js
@@ -8,8 +8,51 @@ const operations = {
 }
 
 models.Base.watch()
-    .on('change', data => {
-        if (data.operationType === operations.INSERT) {
-            
-        }
-    });
+.on('change', data => {
+    if (data.operationType === operations.INSERT) {
+        var base = data.fullDocument;
+        AirOperator.updateById(base.airOperator, {$push: {'bases': base._id}});
+    }
+});
+
+models.Personnel.watch()
+.on('change', data => {
+    if (data.operationType === operations.INSERT) {
+        var personnel = data.fullDocument;
+        // Aggiorno le occupazioni in base
+        if (personnel.roles.occupation.includes('pilot'))
+            Base.updateById(personnel.base, {$push: {'staff.pilots': personnel._id}});
+        if (personnel.roles.occupation.includes('crew'))
+            Base.updateById(personnel.base, {$push: {'staff.crew': personnel._id}});
+        if (personnel.roles.occupation.includes('maintainer'))
+            Base.updateById(personnel.base, {$push: {'staff.maintainers': personnel._id}});
+
+        // Aggiorno i ruoli di comando
+        if (personnel.roles.command.airOperator.includes('AM'))
+            AirOperator.updateById(personnel.airOperator, {'roles.AM': personnel._id});
+        if (personnel.roles.command.airOperator.includes('CQM'))
+            AirOperator.updateById(personnel.airOperator, {'roles.CQM': personnel._id});
+        if (personnel.roles.command.airOperator.includes('SM'))
+            AirOperator.updateById(personnel.airOperator, {'roles.SM': personnel._id});
+        if (personnel.roles.command.base.includes('ViceAM'))
+            Base.updateById(personnel.base, {'roles.ViceAM': personnel._id});
+        if (personnel.roles.command.base.includes('BaseSupervisor'))
+            Base.updateById(personnel.base, {'roles.BaseSupervisor': personnel._id});
+    }
+});
+    
+models.Drone.watch
+.on('change', data => {
+    if (data.operationType === operations.INSERT) {
+        var drone = data.fullDocument;
+        Base.updateById(drone.base, {$push: {drones: drone._id}});
+    }
+});
+
+models.Mission.watch
+.on('change', data => {
+    if (data.operationType === operations.INSERT) {
+        var mission = data.fullDocument;
+        Personnel.updateById(mission.supervisor, {$push: {'missions.supervisor.pending': mission._id}});
+    }
+});
