4a66044d5dd83f41ad9a1cba0f98aa68054db07d
Unknown <alessio.gilardi91@gmail.com>
2018-09-22 21:37:49 +0200

    Prove varie

--- mongodb_prova.js
+++ mongodb_prova.js
@@ -1,14 +1,19 @@
 const models    = require('./models.js');
 const db        = require('./db-connect.js');
 const queries   = require('./queries.js');
+const docs      = require('./db-documents.js');
 
 var mongoose = db.connect();
 
 //queries.AirOperator.insert('eurodrone', 'Italy', 'Genoa', 'Corso Europa 22');
+//queries.AirOperator.insert(docs.AirOperators[0]);
+//queries.Base.insert(docs.Bases[1]);
+//queries.Personnel.insert(docs.Personnel[0]);
+queries.Personnel.insert(docs.Personnel[1]);
 
 //queries.Base.insert2("Base 4", 'eurodrone' , "Italy", "La Speza", "22", "22");
 
-queries.Personnel.insert('42', 'Alessio', 'Bollea', 'ilmiocf', 'Italy', 'Genoa', 'nonvelodico', 'eurodrone', 'Base 4', ['pilot','maintainer'], 'AM', ['ViceAM'], false, 'licId123', 'SuperLicenseType', 5, new Date(), 'LM');
+//queries.Personnel.insert('42', 'Alessio', 'Bollea', 'ilmiocf', 'Italy', 'Genoa', 'nonvelodico', 'eurodrone', 'Base 4', ['pilot','maintainer'], 'AM', ['ViceAM'], false, 'licId123', 'SuperLicenseType', 5, new Date(), 'LM');
 //aIdTelegram, aName, aSurname, aCf, aCountry, aCity, aAddress, aAirOperatorName, aBaseName, aOccupation, aAirOperatorRole = undefined, aBaseRole = undefined, aLocPermission = false, aLicenseId = undefined, aLicenseType = undefined, aLicenseMaxMissionRank = undefined, aLicenseExpireDate = undefined, aDroneTypes = undefined
 //queries.Drone.insert('1lm10dr0n3', 'Heavy', 'eurodrone', 'Base 4', 'Ready');
 
