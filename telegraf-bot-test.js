645ac04ab9c5a4cdd46a51731e25dbb452b79cb5
alessiogilardi <alessio.gilardi91@gmail.com>
2018-09-26 14:53:19 +0200

    Prove

--- telegraf-bot-test.js
+++ telegraf-bot-test.js
@@ -1,47 +1,32 @@
 require('dotenv').config();
-const Telegraf = require('telegraf');
-require('./db-connect.js').connect();
-const queries = require('./queries.js');
-const deasync = require('deasync');
+const Telegraf = require('telegraf')
 const session = require('telegraf/session')
+const Stage = require('telegraf/stage')
+const Scene = require('telegraf/scenes/base')
+const { leave } = Stage
 
-const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
-bot.context.prova = 0;
-bot.use(session());
-// Dizionario che contiene per ogni ruolo la lista di operazioni (uniche) che può fare (probabilmente inutile, ma memorizza tutti i comandi esistenti e chi li può eseguire)
-var role_to_operation = {
-	AM: ['/requestMission'],
-	BS: ['/createMission', '/acceptMission', '/addChiefPilot', '/addCoPilot', '/addCrew', '/addQtb'],
-	pilot: ['/addLogbook'],
-	crew: ['/accept', '/refuse']																// Usabili anche dai piloti
-};
+// Greeter scene
+const greeter = new Scene('greeter')
 
-bot.start((ctx) => ctx.reply('Telegram bot started!'));
-bot.help((ctx) => ctx.reply('Command list: '+findPermissions(ctx)));
-bot.hears('Hi', (ctx) => {
-	ctx.session.pippo = ctx.session.pippo || 0;
-  	ctx.session.pippo++;
-  	return ctx.reply('Message counter: ' + ctx.session.pippo);
+greeter.enter((ctx) => {
+  ctx.reply('Hi');
 });
+greeter.leave((ctx) => ctx.reply('Bye'))
+//greeter.hears(/hi/gi, leave())
+greeter.hears('Hi', (ctx) => ctx.reply('Ciao siamo nella scena greeter'))
+greeter.on('message', (ctx) => ctx.reply('Send `hi`'))
 
-bot.startPolling();
+// Create scene manager
+const stage = new Stage()
+stage.command('cancel', leave())
 
-findPermissions = (ctx) => {
-	var commandList = [];																		// Lista di comandi eseguibili da una certa persona
-	// Recupero il documento della persona a partire dal suo id telegram
-	projection = 'roles.command.airOperator.AM roles.command.base.supervisor roles.occupation.pilot roles.occupation.crew';
-	queries.Personnel.findByIdTelegram(ctx.message.from.id, projection, aPerson => {
-		if (aPerson.roles.command.airOperator.AM)
-			Array.prototype.push.apply(commandList, role_to_operation.AM)
-		if (aPerson.roles.command.base.supervisor)
-			Array.prototype.push.apply(commandList, role_to_operation.BS)
-		if (aPerson.roles.occupation.pilot)
-			Array.prototype.push.apply(commandList, role_to_operation.pilot)
-		if (aPerson.roles.occupation.pilot || aPerson.roles.occupation.crew)
-			Array.prototype.push.apply(commandList, role_to_operation.crew)
-	});
-	while (commandList.length === 0)
-		deasync.runLoopOnce();
-	console.log(commandList);
-	return commandList;																			// non può funzionare per il solito motivo
-};
+// Scene registration
+stage.register(greeter)
+
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN)
+bot.use(session())
+bot.use(stage.middleware())
+bot.command('greeter', (ctx) => ctx.scene.enter('greeter'))
+bot.hears('Hi', ctx => ctx.reply('Saluto generico'))
+
+bot.startPolling()
