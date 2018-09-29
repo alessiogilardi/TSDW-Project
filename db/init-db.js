27423aaaf4e8be155f30e8d235698978685f4843
alessiogilardi <alessio.gilardi91@gmail.com>
2018-09-29 19:47:45 +0200

    Rinomina

--- db/init-db.js
+++ db/init-db.js
@@ -12,11 +12,14 @@ docs.Bases.forEach(base => {
     queries.Base.insert(base);
 });
 */
+/*
 docs.Personnel.forEach(person => {
     queries.Personnel.insert(person);
 });
+*/
 //queries.Base.insert(docs.Bases[1])
 
 //queries.Personnel.insert(docs.Personnel[1]);
 //queries.Personnel.findByIdTelegram(33017299, {}, aPerson => console.log(aPerson))
 
+docs.Drones.forEach(drone => queries.Drone.insert(drone))
