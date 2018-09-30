e088538b4b43c85da37bd322c04bf79db3613d9e
alessiogilardi <alessio.gilardi91@gmail.com>
2018-09-30 22:00:27 +0200

    Aggiunte note sulle cose che mancano da implementare

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 require('dotenv').config();
 require('./db/db-connect.js').connect();
 const Telegraf = require('telegraf');
-const queries = require('./db/queries.js');
-const deasync = require('deasync');
 const session = require('telegraf/session')
 const dataLoader = require('./bot/data-loader-middleware');
 const bf = require('./bot/bot-functions.js');
 const Stage = require('telegraf/stage')
-const Scene = require('telegraf/scenes/base')
 const createMission = require('./bot/commands/create-mission');
 const eventEmitters = require('./event-emitters')
 const { enter, leave } = Stage
@@ -22,10 +19,18 @@ eventEmitters.Mission.on('insert', () => {
 // TODO: potrebbe essere possibile creare un middleware che controlla l'input,
 // se è un comando allora verifica che l'utente sia autorizzato a lanciarlo altrimeni restituisce errore
 
-const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
+// Il personale notificato a seguito della creazione di una missione 
+// potrebbe non essere diviso per ruolo(pilota, crew, ...), in questo modo viene semplicemente mostrato al 
+// supervisore ed è lui a scegliere come distribuire il personale
+// Oppure scelgo tra i piloti quelli adatti alla missione e il personale, poi il supervisore può decidere di assegnare 
+// un pilota alla crew ma non uno della crew ai piloti
+
+// TODO: definire la possibilità che sia droni che piloti siano occupati in un altra missione per cui è inutile notificarli
+
 const stage = new Stage([createMission])
 stage.command('cancel', leave())
 
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
 // session({ ttl: 10 })
 bot.use(session());
@@ -34,12 +39,12 @@ bot.use(stage.middleware())
 
 
 bot.start(ctx => {
-	if (!ctx.session.userData.person.telegramData.botStarted) {
-		ctx.reply(`Ciao ${ctx.message.from.first_name}!`)
-		.then(() => ctx.reply(`Command list:\n ${ctx.session.userData.commands.join('\n')}`))
-		.catch(err => console.log(err));
-		bf.setBotStarted(ctx.message.from.id)
-	}
+	if (ctx.session.userData.person.telegramData.botStarted)
+		return
+	ctx.reply(`Ciao ${ctx.message.from.first_name}!`)
+	.then(() => ctx.reply(`Command list:\n ${ctx.session.userData.commands.join('\n')}`))
+	.catch(err => console.log(err));
+	bf.setBotStarted(ctx.message.from.id)
 });
 bot.help(ctx => ctx.reply(`Command list:\n ${ctx.session.userData.commands.join('\n')}`));
 // Da verificare il tipo di action
