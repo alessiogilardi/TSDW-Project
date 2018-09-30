dc3f0fbbeedd9dcb26536f06df51721aa5d62bfa
alessiogilardi <alessio.gilardi91@gmail.com>
2018-09-30 12:48:54 +0200

    Refactory

--- bot/data-loader-middleware.js
+++ bot/data-loader-middleware.js
@@ -1,15 +1,15 @@
 const bf = require('./bot-functions.js');
 
 const middleware = () => (ctx, next) => {
-    if (!ctx.session.dataLoaded || !ctx.session.isValid)
-        bf.loadData(ctx.message.from.id, data => {
-            ctx.session.userData    = data;
-            ctx.session.dataLoaded  = true;
-            ctx.session.isValid     = true;
-            return next();
-        });
-    else
+    if (ctx.session.dataLoaded && ctx.session.isValid)
+        return next()
+    
+    bf.loadData(ctx.message.from.id, data => {
+        ctx.session.userData    = data;
+        ctx.session.dataLoaded  = true;
+        ctx.session.isValid     = true;
         return next();
+    });
 }
 
 module.exports = middleware;
