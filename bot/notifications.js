b731adcc6d55beca9aff6a621a5184bab4850057
alessiogilardi <alessio.gilardi91@gmail.com>
2018-10-01 22:05:49 +0200

    Uso array alreadyNotified

--- bot/notifications.js
+++ bot/notifications.js
@@ -4,7 +4,9 @@ const queries = require('../db/queries')
 exports.build = () => (telegramBot) => {
     eventEmitters.Mission.on('insert', aMission => {
         queries.Drone.findById(aMission.drones[0], 'type', aDrone => {
+            var alreadyNotified = [aMission.supervisor]
             queries.Personnel.find({
+                _id: {$nin: alreadyNotified},
                 base: aMission.base,
                 'roles.occupation.pilot': true,
                 'pilot.license.maxMissionRank': {$gte: aMission.rank}, 
@@ -12,35 +14,32 @@ exports.build = () => (telegramBot) => {
                 {}, 
                 pilots => {
                     // TODO: recuperare anche le ore di volo dei piloti
-                    
-                    // Notifico i piloti
+
+                    // Notifico i piloti 
                     pilots.forEach(pilot => telegramBot.sendMessage(pilot.telegramData.idTelegram, 'Notifica di missione, /accept o /decline ?'))
 
-                    var pilotsIds = []
-                    pilots.forEach(pilot => pilotsIds.push(pilot._id));
+                    pilots.forEach(pilot => alreadyNotified.push(pilot._id));
 
                     queries.Personnel.find({
                         base: aMission.base,
                         'roles.occupation.crew': true,
-                        _id: {$nin: pilotsIds}},
+                        _id: {$nin: alreadyNotified}},
                         {},
                         crew => {
                             // Notifico la crew
                             crew.forEach(crewMember => telegramBot.sendMessage(crewMember.telegramData.idTelegram, 'Notifica di missione, /accept o /decline ?'))
 
-                            if (aMission.duration.expected >= 3) {
-                                var crewIds = []
-                                crew.forEach(crewMember => crewIds.push(crewMember._id))
+                            if (aMission.duration.expected >= 3) { // Se la durata è maggiore di 3h
+                                crew.forEach(crewMember => alreadyNotified.push(crewMember._id))
 
                                 queries.Personnel.find({
                                     base: aMission.base,
                                     'roles.occupation.crew': true,
                                     _id: {$nin: pilotsIds.concat(crewIds)}},
                                     {},
-                                    maintainers => {
-                                        // Notifico i manutentori
-                                        maintainers.forEach(maintainer => telegramBot.sendMessage(maintainer.telegramData.idTelegram, 'Notifica di missione, /accept o /decline ?'))
-                                })
+                                    // Notifico i manutentori
+                                    maintainers => maintainers.forEach(maintainer => telegramBot.sendMessage(maintainer.telegramData.idTelegram, 'Notifica di missione, /accept o /decline ?'))
+                                )
                             }
 
                     })
