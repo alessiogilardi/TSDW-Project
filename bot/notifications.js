7e7841cd204f312220ccdba6b00e9f3e0e707617
alessiogilardi <alessio.gilardi91@gmail.com>
2018-10-02 10:19:33 +0200

    Debugged, not tested

--- bot/notifications.js
+++ bot/notifications.js
@@ -3,26 +3,42 @@ const queries       = require('../db/queries')
 
 const findAnNotify = aMission => {
     var alreadyNotified = [aMission.supervisor]
-    queries.Personnel.find(eventHandlers.Mission.queries.pilots, {}, pilots =>{
+    queries.Personnel.find({
+            _id: {$nin: alreadyNotified},
+            base: aMission.base,
+            'roles.occupation.pilot': true,
+            'pilot.license.maxMissionRank': {$gte: aMission.rank}, 
+            'pilot.droneTypes': {$all: [aMission.drones[0].type]}
+        }, {}, pilots =>{
         // TODO: recuperare anche le ore di volo dei piloti
 
         // Notifico i piloti
         pilots.forEach(pilot => {
-            telegramBot.sendMessage(pilot.telegramData.idTelegram, 'Notifica di missione come pilota, /accept o /decline ?')
+            this.bot.sendMessage(pilot.telegramData.idTelegram, 'Notifica di missione come pilota, /accept o /decline ?')
             alreadyNotified.push(pilot._id)
         })
 
-        queries.Personnel.find(eventHandlers.Mission.queries.crew, {}, crew => {
+        queries.Personnel.find({
+                base: aMission.base,
+                'roles.occupation.crew': true,
+                _id: {$nin: this.alreadyNotified}
+            }, {}, crew => {
             // Notifico la crew
             crew.forEach(crewMember => {
-                telegramBot.sendMessage(crewMember.telegramData.idTelegram, 'Notifica di missione come membro dell\'equipaggio, /accept o /decline ?')
+                this.bot.sendMessage(crewMember.telegramData.idTelegram, 'Notifica di missione come membro dell\'equipaggio, /accept o /decline ?')
                 alreadyNotified.push(crewMember._id)
             })
 
             // Se la missione dura più di 3h cerco anche i maintainers
             if (aMission.descrtiption.duration.expected >= 3)
-                queries.Personnel.find(eventHandlers.Mission.queries.maintainers, {}, maintainers => 
-                    maintainers.forEach(maintainer => telegramBot.sendMessage(maintainer.telegramData.idTelegram, 'Notifica di missione come manutentore, /accept o /decline ?')))
+                queries.Personnel.find({
+                    _id: {$nin: this.alreadyNotified},
+                    base: aMission.base,
+                    'roles.occupation.crew': true,
+                }, {}, maintainers => 
+                    maintainers.forEach(maintainer => this.bot.sendMessage(maintainer.telegramData.idTelegram, 'Notifica di missione come manutentore, /accept o /decline ?')))
+            
+            //alreadyNotified = []
         })
 
     })
@@ -30,35 +46,16 @@ const findAnNotify = aMission => {
 
 const eventHandlers = {
     Mission: {
-        // TODO: escludere dalla ricerca i piloti occupati in missione
-        queries: {
-            pilots: {
-                _id: {$nin: alreadyNotified},
-                base: aMission.base,
-                'roles.occupation.pilot': true,
-                'pilot.license.maxMissionRank': {$gte: aMission.rank}, 
-                'pilot.droneTypes': {$all: [aMission.drones[0].type]}
-            },
-            crew: {
-                base: aMission.base,
-                'roles.occupation.crew': true,
-                _id: {$nin: alreadyNotified}
-            },
-            maintainers: {
-                _id: {$nin: alreadyNotified},
-                base: aMission.base,
-                'roles.occupation.crew': true,
-            }
-
-        },
         insert: () => eventEmitters.Mission.on('insert', aMission => findAnNotify(aMission))
     }
 }
 
 
 
-const notification = telegramBot => {
-    this.telegramBot = telegramBot
+const notification = bot => {
+    if (bot === null || bot === undefined) throw new Error('Missing Telegram Bot')
+    this.bot = bot
+
     eventHandlers.Mission.insert()
 }
 
