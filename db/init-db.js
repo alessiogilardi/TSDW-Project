504681009eb7af28464cfd27d14dd5bfbe33c10b
alessiogilardi <alessio.gilardi91@gmail.com>
2018-10-03 17:15:16 +0200

    Ora funzione in automatico

--- db/init-db.js
+++ db/init-db.js
@@ -2,8 +2,16 @@ require('./db-connect.js').connect()
 const models    = require('./models.js')
 const queries   = require('./queries.js')
 const docs      = require('./db-documents.js')
+const eventEmitters = require('../event-emitters')
 
-//ocs.AirOperators.forEach(airOperator => queries.AirOperator.insert(airOperator))
-//docs.Bases.forEach(base => queries.Base.insert(base))
-//docs.Personnel.forEach(person => queries.Personnel.insert(person))
-docs.Drones.forEach(drone => queries.Drone.insert(drone))
+var count = 0;
+
+docs.AirOperators.forEach(airOperator => queries.AirOperator.insert(airOperator))
+eventEmitters.AirOperator.on('insert', airOperator =>docs.Bases.forEach(base => queries.Base.insert(base)))
+eventEmitters.Base.on('insert', base => {
+    count++
+    if (count === docs.Bases.length) {
+        docs.Personnel.forEach(person => queries.Personnel.insert(person))
+        docs.Drones.forEach(drone => queries.Drone.insert(drone))
+    }
+})
