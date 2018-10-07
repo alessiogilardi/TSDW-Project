e91816594f62370c3cb8b719f35db069a186ed4f
alessiogilardi <alessio.gilardi91@gmail.com>
2018-10-07 22:54:43 +0200

    Debug e modifche minori

--- bot/commands/request-mission.js
+++ bot/commands/request-mission.js
@@ -1,29 +1,23 @@
 const WizardScene   = require('telegraf/scenes/wizard/index')
 const Composer      = require('telegraf/composer')
 const queries       = require('../../db/queries')
-const schemas       = require('../../db/schemas')
 const eventEmitters = require('../../event-emitters')
 
 // TODO: va formattato l'output quando mostro i droni disponibili (decidere cosa mostrare e come)
+// TODO: la data va formattata in output
 
 const dataStructure = {
     name: 'requestMission',
     error: false,
     searching: false,
     params: {
-        baseName: undefined,
-        baseSupervisor: undefined,
+        base: {
+            name: undefined,
+            supervisor: undefined
+        },
         date: undefined,
         description: undefined
     }
-}
-
-const arrayContainsArray = (superset, subset) => {
-    if (0 === subset.length || superset.length < subset.length) return false
-    subset.forEach(subVal => {
-        if (!superset.includes(subVal)) return false
-    })
-    return true
 }
 
 const requestMission = new WizardScene('requestMission',
@@ -38,7 +32,7 @@ const requestMission = new WizardScene('requestMission',
     new Composer()
     .on('text', ctx => {
         // Recupero il testo e verifico che sia una data
-        // TODO: la data deve essere inseribile in un formato più elastico oppure deve essere spiagto il formato in cui inserirla
+        // TODO: la data deve essere inseribile in un formato più elastico oppure deve essere spiegato il formato in cui inserirla
         // TODO: la data deve essere successiva ad oggi
         ctx.session.command.params.date = Date.parse(ctx.message.text)
         if (isNaN(ctx.session.command.params.date)) {
@@ -51,14 +45,17 @@ const requestMission = new WizardScene('requestMission',
     new Composer()
     .on('text', ctx => {
         // Cerco la base
-        // TODO: mentre cerco devo scartare l'input, altrimenti se vengono mandati più messaggi partono molteplici ricerche
+        if (ctx.session.command.searching)
+            return
+        ctx.session.command.searching = true
         queries.Base.findByName(ctx.message.text, {}, aBase => {
+            ctx.session.command.searching = false
             if (aBase == null) {
                 ctx.reply('Mi spiace hai inserito una base non valida, per favore inseriscine un\' altra.')
                 return
             }
-            ctx.session.command.params.baseName = aBase.name;
-            ctx.session.command.params.baseSupervisor = aBase.roles.supervisor;
+            ctx.session.command.params.base.name = aBase.name;
+            ctx.session.command.params.base.supervisor = aBase.roles.supervisor;
             ctx.reply('Infine, scrivi una breve descrizione della missione, per far sapere di cosa si tratta.')
             .catch(err => console.log(err))
             return ctx.wizard.next()
@@ -75,15 +72,13 @@ const requestMission = new WizardScene('requestMission',
             return
         }
         // TODO: non sono sicuro sia il caso di renotificare chi l'ha richiesta visto che è un dato che non teniamo nel DB
-        ctx.reply('La richiesta è stata inoltrata con successo. Verrai notificato nel caso le missione verrà accettata')
-        .then(ctx.reply(`Ecco intanto un riepilogo sui dati della missione\n\n${JSON.stringify(ctx.session.command)}`))
+        ctx.reply('La richiesta è stata inoltrata con successo. Verrai notificato nel caso la missione venga accettata.')
+        .then(ctx.reply(`Ecco intanto un riepilogo sui dati della missione:\n\nData prevista: ${ctx.session.command.params.date}\nBase di partenza: ${ctx.session.command.params.base.name}\nResponsabile: ${ctx.session.command.params.base.supervisor}\nDescrizione: ${ctx.session.command.params.description}`))
         .catch(err => console.log(err))
         // Qua mando il messaggio al base supervisor che deve ricevere la notifica
         // Estraggo il telegramId a partire dall'id del supervisore e mando il messaggio con i dati appena inseriti della missione richiesta
-        queries.Personnel.findById(ctx.session.command.params.baseSupervisor, {}, aPerson => {
-            //ctx.sendMessage(aPerson.telegramData.idTelegram, `E' stata richiesta una missione con i seguenti dati:\n
-            //${JSON.stringify(ctx.session.command)}`) // NON ESISTE ctx.sendMessage(), bisogna trovare soluzione
-            eventEmitters.Bot.emit('requestMission', aPerson, message)
+        queries.Personnel.findById(ctx.session.command.params.base.supervisor, {}, aPerson => {
+            eventEmitters.Bot.emit('requestMission', aPerson, `E' stata richiesta una missione con i seguenti dati:\nData prevista: ${ctx.session.command.params.date}\nBase di partenza: ${ctx.session.command.params.base.name}\nDescrizione: ${ctx.session.command.params.description}`)
         });
 });
 
