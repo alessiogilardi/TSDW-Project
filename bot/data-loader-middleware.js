71fc798cd1e85b025aa04724297629739ae2983d
alessiogilardi <alessio.gilardi91@gmail.com>
2018-10-07 22:56:20 +0200

    Cambiato da from.id a chat.id tendando di fixare bug

--- bot/data-loader-middleware.js
+++ bot/data-loader-middleware.js
@@ -3,8 +3,7 @@ const bf = require('./bot-functions.js');
 const middleware = () => (ctx, next) => {
     if (ctx.session.dataLoaded && ctx.session.isValid)
         return next()
-    
-    bf.loadData(ctx.message.from.id, data => {
+    bf.loadData(ctx.message.chat.id, data => {
         ctx.session.userData    = data;
         ctx.session.dataLoaded  = true;
         ctx.session.isValid     = true;
