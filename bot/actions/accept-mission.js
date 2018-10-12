c58e54c306988e0129f8a54e54729d1881dd6249
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2018-10-12 17:23:40 +0200

    Update modulo accept mission

--- bot/actions/accept-mission.js
+++ bot/actions/accept-mission.js
@@ -1,7 +1,19 @@
+const queries =  require('../../db/queries')
+/**
+ * Definisco i passi da fare per accettare una missione:
+ * - 
+ */
+
+
+
+
 const accept = () => ctx => (ctx, next) => {
+
+
+
     console.log('Missione accettata')
 	ctx.answerCbQuery('Missione accettata')
-	ctx.editMessageReplyMarkup({}) 
+	ctx.editMessageReplyMarkup({})
 }
 
 module.exports = accept
