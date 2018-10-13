b44867115df70a7f1c77ab0bc33708051eaafba4
alessiogilardi <alessio.gilardi91@gmail.com>
2018-10-13 12:21:09 +0200

    Rimossi

--- bot/actions/decline-mission.js
+++
@@ -1,7 +0,0 @@
-const decline = () => ctx => (ctx, next) => {
-    console.log('Missione rifiutata')
-	ctx.answerCbQuery('Missione rifiutata')
-	ctx.editMessageReplyMarkup({})
-}
-
-module.exports = decline
