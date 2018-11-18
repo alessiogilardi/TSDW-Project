922c4bf86f1b2b72b5b2afa4a0f3fd33d3572a40
alessiogilardi <alessio.gilardi91@gmail.com>
2018-11-18 13:13:47 +0100

    Backup

--- db/prove-query.js
+++ db/prove-query.js
@@ -1,4 +1,12 @@
-require('./db-connect').connect()
+var db = require('./db-connect').connect()
 const queries = require('./queries')
+/*
+queries.Mission.update({_id: "5bc19aa57b92062afcafa30b"}, {date: new Date})
+.then(() => console.log('Query eseguita'))
+.catch(err => console.log('Query fallita'))
+*/
 
-queries.Personnel.find({name: 'Alessio'}, 'name surname telegramData', (err, aPerson) => console.log(aPerson))
+//queries.Mission.Pilot.setAsNotified("5bc19aa57b92062afcafa30b", "5bbe535d2652152130333947")
+queries.Mission.Pilot.setAsAccepted("5bc19aa57b92062afcafa30b", "5bbe535d2652152130333947", new Date("2018-11-07T13:47:24.55Z"))
+.then(() => console.log("Query eseguita"))
+.catch(() => console.log("Query non eseguita"))
