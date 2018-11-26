f556cd9b29632fe65a4630e510d777946d93b104
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2018-11-26 14:33:15 +0100

    Ricreato

--- bot/commands/request-mission.js
+++ bot/commands/request-mission.js
@@ -1,41 +1,54 @@
-/**
- * Modulo che gestisce la richista di una missione da parte di un AM nei confronti di un responsabile di base.
- * Utilizza una Scene implmentata attraverso vari step consecutivi.
- * Al completamento di tutti gli step la Scene viene lasciata e viene richiesta missione.
- * Viene lanciato un evento requestMission gestuto dal modulo onRequestMission
- * 
- * Se è lanciato il comando /cancel la procedura viene interrotta, i dati inseriti vengono scartati
- * e la Scene è lasciata senza apportare modifiche.
- */
-
-
-
+require('dotenv').config()
 const WizardScene   = require('telegraf/scenes/wizard/index')
 const Composer      = require('telegraf/composer')
 const queries       = require('../../db/queries')
+const schemas       = require('../../db/schemas')
 const eventEmitters = require('../../events/event-emitters')
 const utils         = require('../../utils')
-// TODO: va formattato l'output quando mostro i droni disponibili (decidere cosa mostrare e come)
-// TODO: la data va formattata in output
-// TODO: non rinotificare chi ha richiesto la missione
+//const Telegraf      = require('telegraf')
 
-const dataStructure = {
-    name: 'requestMission',
-    error: false,
-    searching: false,
-    params: {
-        base: {
-            name: undefined,
-            supervisor: undefined
-        },
-        date: undefined,
-        description: undefined
+const Base    = queries.Base
+const Mission = queries.Mission
+//const Personnel = queries.Personnel
+//const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN)
+//const sendMessage = bot.telegram.sendMessage
+
+/* Modello da seguire, non c'è bisogno di dichiararlo:
+    const dataStructure = {
+        name: 'requestMission',
+        error: false,
+        searching: false,
+        params: {
+            base: {
+                name: undefined,
+                supervisor: undefined
+            },
+            date: undefined,
+            description: undefined
+        }
     }
-}
+*/
 
+/**
+ * Si richiedono:
+ *  - Data della missione
+ *  - Base di partenza
+ *  - Location di inizio della missione
+ *  - Descrizione
+ *      - Durata prevista -> usata per generare più missioni e aggiungere Manutentori
+ *      - Valutazione di rischio
+ * 
+ * Azioni da eseguire in seguito:
+ *  - Settare a true il campo Instantiated della missione
+ *  - Aggiungere il timestamp a tale campo
+ *  - Inserire l'evento nel eventsLog
+ *  - Notificare il baseSup con i parametri della Missione
+ */
 const requestMission = new WizardScene('requestMission',
     ctx => {
-        ctx.session.command = dataStructure
+        ctx.session.command.name      = 'requestMission'
+        ctx.session.command.searching = false
+        ctx.session.command.error     = false
         ctx.reply('Bene, iniziamo la procedura per la richiesta di una missione!\nTi verrà chiesto di inserire alcuni parametri.')
         .then(() => ctx.reply('Ti ricordo che puoi annullare l\'operazione  in qualsiasi momento usando il comando /cancel.'))
         .then(() => ctx.reply('Inserisci la data della missione:'))
@@ -43,9 +56,8 @@ const requestMission = new WizardScene('requestMission',
         ctx.wizard.next()
     },
     new Composer()
-    .on('text', ctx => {
-        // Parso la data inserita e la verifico                
-        if (!utils.Date.isValid(ctx.message.text)) {
+    .on('text', ctx => {            
+        if (!utils.Date.isValid(ctx.message.text)) { // Verifico che la data inserita sia in un formato corretto
             ctx.reply('La data inserita non è valida, per favore reinseriscila')
             return
         }
@@ -55,43 +67,112 @@ const requestMission = new WizardScene('requestMission',
     }),
     new Composer()
     .on('text', ctx => {
-        // Cerco la base
         if (ctx.session.command.searching)
             return
         ctx.session.command.searching = true
-        queries.Base.findByName(ctx.message.text, {}, aBase => {
+        /*
+        Base.findByName(ctx.message.text, {})
+        .then(aBase => {
+
+        })
+        */
+        Base.findByName(ctx.message.text, {}, aBase => {
             ctx.session.command.searching = false
-            if (aBase == null) {
-                ctx.reply('Mi spiace hai inserito una base non valida, per favore inseriscine un\' altra.')
+            if (aBase === null) {
+                ctx.reply('Mi spiace, hai inserito una base non valida, per favore inseriscine un\'altra.')
                 return
             }
             ctx.session.command.params.base.name = aBase.name;
             ctx.session.command.params.base.supervisor = aBase.roles.supervisor;
-            ctx.reply('Infine, scrivi una breve descrizione della missione, per far sapere di cosa si tratta.')
-            .catch(err => console.log(err))
+
+            ctx.reply('Inserisci le coordinate dove avrà luogo la missione.\nInserisci una posizione da GPS oppure scrivile in formato UTM (ZoneNumber ZoneLetter Easting Northing):')
             return ctx.wizard.next()
-        });
+        })
     }),
     new Composer()
-    .on('text', ctx => {
-        ctx.session.command.params.description = ctx.message.text
-        return ctx.scene.leave()
-    }))
-    .leave(ctx => {
-        if (ctx.message.text === '/cancel') {
-            ctx.reply('Richiesta missione annullata.')
+    .on('text', ctx => { // Leggo la location in coordinate UTM
+        var coordinates = utils.stringToUTM(ctx.message.text) // Converto in coordinate UTM
+        if (coordinates === null) {
+            ctx.reply('Mi spiace, le coordinate che hai inserito non sono valide, reinseriscile.')
             return
         }
-        // TODO: non sono sicuro sia il caso di rinotificare chi l'ha richiesta visto che è un dato che non teniamo nel DB
-        ctx.reply('La richiesta è stata inoltrata con successo. Verrai notificato nel caso la missione venga accettata.')
-        .then(ctx.reply(`Ecco intanto un riepilogo sui dati della missione:\n\nData prevista: ${utils.Date.format(ctx.session.command.params.date, 'DD-MM-YYYY')}\nBase di partenza: ${ctx.session.command.params.base.name}\nResponsabile: ${ctx.session.command.params.base.supervisor}\nDescrizione: ${ctx.session.command.params.description}`))
-        .catch(err => console.log(err))
+        coordinates = utm.convertUtmToLatLng(coordinates.easting, coordinates.northing, coordinates.zoneNum, coordinates.zoneLetter);
+        ctx.session.command.params.location.latitude  = coordinates.lat
+        ctx.session.command.params.location.longitude = coordinates.lng
 
-        // Mando il messaggio al base supervisor che deve ricevere la notifica
-        queries.Personnel.findById(ctx.session.command.params.base.supervisor, {}, aSupervisor => {
-            eventEmitters.Bot.emit('requestMission', {supervisor: aSupervisor, params: ctx.session.command.params})
-        })
+        ctx.reply('Inserisci la durata prevista:')
+        return ctx.wizard.next()
+
+    })/*
+    .on('location', ctx => { // Inserisco la location da GPS mediante modulo Telegram
+
+    })*/,
+    new Composer()
+    .on('text', ctx => { // Leggo la durata prevista
+        if (isNaN(ctx.message.text)) { // Verifico che il valore inserito sia un valore numerico
+            ctx.reply('Mi spiace hai inserito un valore non valido, per favore inseriscine un altro.')
+            return
+        }
+        ctx.command.params.description.duration.expected = ctx.message.text
+
+        ctx.reply('Inserisci uno scenario valido:')
+        return ctx.wizard.next()
+    }),
+    new Composer()
+    .on('text', ctx => { // Leggo lo scenario A,B,C
+        if (!schemas.scenarios.includes(ctx.message.text)) {
+            ctx.reply('Scenario non valido, inseriscine un altro.')
+            return
+        }
+        ctx.session.command.params.description.riskEvaluation.scenario = ctx.message.text
+
+        ctx.reply('Inserisci la difficoltà della missione (1 - 4):')
+        return ctx.wizard.next()
+    }),
+    new Composer()
+    .on('text', ctx => { // Leggo il riskLevel della missione
+        // Controllo che il valore inserito sia numerico e che sia un valore valido
+        if (isNaN(ctx.message.text) || 
+                ctx.message.text < schemas.riskLevel.min || 
+                ctx.message.text > schemas.riskLevel.max) {
+            ctx.reply('Ops, la difficoltà che hai inserito non è valida, inserisci un valore diverso.')
+            return
+        }
+        ctx.session.command.params.description.riskEvaluation.level = ctx.message.text
+
+        return ctx.scene.leave()
+    })
+).leave(ctx => {
+    if (ctx.message.text === '/cancel') {
+        ctx.reply('Richiesta missione annullata.')
+        delete ctx.session.command
+        return
+    }
+
+    ctx.reply('La missione è stata creata con successo!\nSarai notificato appena il Responsabile di Base l\'avrà presa in carico.')
+    //.then(ctx.reply(`Ecco intanto un riepilogo sui dati della missione\n\nData: ${ctx.session.command.params.date}\nDurata prevista: ${ctx.session.command.params.expectedDuration}\nRango: ${ctx.session.command.params.rank}\nDroni scelti: ${ctx.session.command.params.drones.chosen.join(', ')}`))
+    .then(() => ctx.reply('Ecco un riepilogo' + ctx.session.command.params))
+    .catch(err => console.log(err))
+
+    var aMission = ctx.session.command.params
+    aMission.status.instantiated.value     = true
+    aMission.status.instantiated.timestamp = new Date()
+
+    ctx.session.command = undefined
+
+    // TEST:
+    return console.log(aMission)
+    //
+
+    // TODO: 
+    // 1 - Inserire la Missione nel DB
+    // 2 - Notificare il responsabile di base
+    // 3 - Inserire l'evento nell'EventLog
+
+    Mission.insert(aMission)
+    .then(mission => {
+        // Notifico il responsabile di base
+        eventEmitters.Bot.emit('requestMission', mission)
+    })
+    .catch(err => console.log(err))
 })
-
-
-module.exports = requestMission
