2f13ed257adefb21e41a744dc9d76ae973bff03d
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2018-11-26 14:35:51 +0100

    Ricreato

--- events/bot/on-request-mission.js
+++ events/bot/on-request-mission.js
@@ -1,7 +1,21 @@
-const onRequestMission = (bot, data) => {
+/**
+ * Modulo che gestisce l'evento onRequestMission.
+ * 
+ * Il modulo si occupa di gestire gli eventi conseguenti all richiesta di una missione:
+ * 1 - Invia una notifcica al baseSup della missione richiedendo di prenderla in carico
+ *      - Recupera il telegramId dal DB
+ *      - Genera il messaggio con un pulsante per l'evento corrispondente
+ * 2 - Inserisco l'evento nell'EventLog 
+ */
+
+
+const onRequestMission = (bot, aMission) => {
     if (bot === undefined || bot === null) throw new Error('Missing Telegram Bot')
+    this.bot = bot
+
+    //
     
-    bot.telegram.sendMessage(data.supervisor, `E' stata richiesta una missione con i seguenti dati:\nData prevista: ${utils.Date.format(data.params.date, 'DD-MM-YYYY')}\nBase di partenza: ${data.params.base.name}\nDescrizione: ${data.params.description}`)
+    //bot.telegram.sendMessage(data.supervisor, `E' stata richiesta una missione con i seguenti dati:\nData prevista: ${utils.Date.format(data.params.date, 'DD-MM-YYYY')}\nBase di partenza: ${data.params.base.name}\nDescrizione: ${data.params.description}`)
     
 }
 
