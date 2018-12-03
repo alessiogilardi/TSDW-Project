f5ce247014a729b7ccb13c12296c8d04a1026ff3
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2018-12-03 21:15:53 +0100

    Update data-loader-middleware.js

--- bot/data-loader-middleware.js
+++ bot/data-loader-middleware.js
@@ -3,17 +3,18 @@
  * Il modulo si occupa di verificare se i dati dell'utente sono già stati caricati in memoria e 
  * nel caso esegue una query al db per caricarli.
  */
-const bf = require('./bot-functions.js');
+const bf = require('./bot-functions.js')
 
 const middleware = () => (ctx, next) => {
     if (ctx.session.dataLoaded && ctx.session.isValid)
         return next()
-    bf.loadData(ctx.message.chat.id, data => {
-        ctx.session.userData    = data;
-        ctx.session.dataLoaded  = true;
-        ctx.session.isValid     = true;
-        return next();
-    });
+    bf.loadData(ctx.message.chat.id)
+    .then(data => {
+        ctx.session.userData    = data
+        ctx.session.dataLoaded  = true
+        ctx.session.isValid     = true
+        return next()
+    })
 }
 
-module.exports = middleware;
+module.exports = middleware
