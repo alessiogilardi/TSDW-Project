be9787a984a1099e6dcc66ab0b04b8d87af8ad77
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2018-12-03 21:16:39 +0100

    Creato  modulo di gestione query button

---
+++ bot/router.js
@@ -0,0 +1,17 @@
+const Router = require('telegraf/router')
+const bf     = require('./bot-functions')
+const unZip  = bf.unZipActionName
+
+const mRouter = new Router(({ callbackQuery }) => {
+    if (!callbackQuery.data) return
+
+    const parts = callbackQuery.data.split(':')
+    return {
+        route: unZip[parts[0]],
+        state: {
+            data: parts.slice(1,parts.length)
+        }
+    }
+})
+
+module.exports = mRouter
