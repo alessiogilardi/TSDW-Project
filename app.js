37a214f38b4dd991d315b54598ffd682d4f5e87a
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2018-12-03 21:15:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,10 +6,11 @@ const session 		 = require('telegraf/session')
 const dataLoader	 = require('./bot/data-loader-middleware');
 const bf 			 = require('./bot/bot-functions.js');
 const Stage 		 = require('telegraf/stage')
-const createMission  = require('./bot/scenes/create-mission');
+const organizeMission  = require('./bot/scenes/organize-mission');
 const requestMission = require('./bot/scenes/request-mission')
 const eventEmitters	 = require('./events/event-emitters')
 const eventRegister  = require('./events/event-register')
+const router		 = require('./bot/router')
 const { enter, leave } = Stage
 
 const backtick = '\`';
@@ -25,7 +26,7 @@ const backtick = '\`';
 
 // TODO: definire la possibilità che sia droni che piloti siano occupati in un altra missione per cui è inutile notificarli
 
-const stage = new Stage([createMission, requestMission])
+const stage = new Stage([organizeMission, requestMission])
 stage.command('cancel', leave())
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
@@ -49,6 +50,7 @@ bot.help(ctx => ctx.reply(`Command list:\n${ctx.session.userData.commands.join('
 // TODO: action -> delete non funziona -> Da verificare il tipo di action
 // bot.action('delete', ctx => bf.resetBotStarted(ctx.message.from.id))
 
+/*
 bot.command(['createMission', 'createmission'], ctx => {
 	if (!ctx.session.userData.commands.includes('/createMission')) {
 		ctx.reply('Mi spiace, non hai i diritti per eseguire questo comando.')
@@ -56,9 +58,7 @@ bot.command(['createMission', 'createmission'], ctx => {
 	}
 	ctx.scene.enter('createMission');
 });
-
-bot.on('location', ctx => console.log(ctx.message))
-
+*/
 bot.command(['requestMission', 'requestmission'], ctx => {
 	if (!ctx.session.userData.commands.includes('/requestMission')) {
 		ctx.reply('Mi spiace, non hai i diritti per eseguire questo comando.')
@@ -67,6 +67,37 @@ bot.command(['requestMission', 'requestmission'], ctx => {
 	ctx.scene.enter('requestMission');
 });
 
+//////////// DEGUGGING DA CANCELLARE ////////////////////
+bot.hears(['A','a'], ctx => {
+    var message = 'Prova'
+    var buttonText = 'Bottone'
+    var buttonData = 'orgMiss:123123131231123123313123'
+    ctx.reply(message, Telegraf.Extra
+    .markdown()
+    .markup(m => m.inlineKeyboard([
+        m.callbackButton(buttonText, buttonData)
+    ])))
+})
+////////////////////////////////////////////////////////
+
+router.on('organizeMission', ctx => {
+	// Entro nella scene: OrganizeMission
+	ctx.scene.enter('organizeMission', { mission: { _id: ctx.state.data[0] } })
+})
+
+router.on('acceptMission', ctx => {
+	const data = ctx.state.data
+	// Entro nella scene: OrganizeMission
+})
+
+router.on('declineMission', ctx => {
+	const data = ctx.state.data
+	// Entro nella scene: OrganizeMission
+})
+
+bot.on('callback_query', router)
+
+/*
 bot.on('callback_query', ctx => {
 	// Rispondo alle actions dei Button ed emetto l'evento appropriato
 	var cbQuery = JSON.parse(ctx.callbackQuery.data)
@@ -74,6 +105,9 @@ bot.on('callback_query', ctx => {
 	ctx.editMessageReplyMarkup({})
 	eventEmitters.bot.emit(cbQuery.action, cbQuery.data, ctx)
 })
-
+*/
 db.connect(process.env.DB_ADDRESS, process.env.DB_PORT, process.env.DB_NAME)
-.then(() => bot.startPolling())
+.then(() => {
+	console.log('Bot started!')
+	bot.startPolling()
+})
