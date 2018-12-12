033dacb4f7a2e0043241bf6f54fe45b675384e78
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2018-12-12 21:05:01 +0100

    Ora ingnora i comandi che non esistono

--- bot/router.js
+++ bot/router.js
@@ -5,9 +5,8 @@ const unZip  = bf.unZip
 const mRouter = new Router(({ callbackQuery }) => {
     if (!callbackQuery.data) return
     const parts = callbackQuery.data.split(':')
-    return {
-        route: unZip[parts[0]],
-        state: { data: parts.slice(1,parts.length) }
+    if (parts[0] in unZip) {
+        return { route: unZip[parts[0]], state: { data: parts.slice(1,parts.length) } }
     }
 })
 
