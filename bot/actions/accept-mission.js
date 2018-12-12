a0da06edf73ed4759db2e5cb4162591af419e68c
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2018-12-12 18:21:44 +0100

    Create accept-mission.js

---
+++ bot/actions/accept-mission.js
@@ -0,0 +1,94 @@
+
+
+const queries   = require('./db/queries')
+const Personnel = queries.Personnel
+const Mission   = queries.Mission
+
+/**
+ * 1. Ricevo _id della missione accetttata tramite callbackButton
+ * 2. Aggiungo la missione a quelle accettate in
+ *  	--> missions.accepted
+ * 3. Aggiungo il Personale che ha accettato alla missione
+ * 		--> rimuovo da personnel.notified
+ * 		--> aggiungo in personnel.accepted
+ * 4. Man mano che le persone accettano controllo quanti hanno
+ * 	  accettato finora e nel caso notifico il baseSup:
+ * 		Se:
+ * 			- La missione dura meno di 3h:
+ * 				1. Ci sono almeno 3 persone
+ * 				2. Almeno 2 possono fare i piloti
+ * 					--> Notifico il baseSup della missione per la scelta del Team
+ * 			- La missione dura più di 3h
+ * 				1. Ci sono almeno 4 persone
+ * 				2. Almeno 2 possono fare i piloti
+ * 				3. Almeno 1 può fare il manutentore
+ * 					--> Notifico il baseSup della missione per la scelta del Team
+ * 
+ * NOTA: il baseSup sceglierà i ruoli che ognuno avrà nella
+ * 		 missione a sua discrezione
+ */
+
+
+const acceptMission = async (bot, ctx) => {
+    const missionId = ctx.state.data[0]
+	const roles 	= ctx.state.data[1].split(',') // Ruoli che può ricoprire nella missione
+	const person 	= ctx.session.userData
+	
+	let aMission 	= await Mission.findById(missionId, '')
+
+	await Personnel.updateById(person._id, { $push: { 'accepted.idMission': aMission._id, date: aMission.date, roles: roles } })
+	await Mission.updateById(aMission._id, { $pull: { 'personnel.notified': person._id } })
+	await Mission.updateById(aMission._id, { $push: { 'personnel.accepted': person._id } })
+
+	// Cerco le persone che hanno accettato
+	let aMission = await Mission.findById(missionId, '')
+	let accepted = aMission.personnel.accepted
+	if (accepted.length < 3) {
+		return
+	}
+	// Se la missione dura meno di 3h
+	if (aMission.description.duration.expected < 3) {
+		let pilotCount = 0
+		for (let person of accepted) {
+			if (person.roles.includes('pilot')) 
+				pilotCount++
+		}
+		if (pilotCount >= 2) {
+			// TODO: Notifico il BaseSup
+		}
+		return
+	}
+
+	// Se la missione dura più di 3h
+	if (accepted.length < 4) {
+		return
+	}
+	let pilotCount = 0
+	let maintCount = 0
+	let pilotAndMaintCount = 0
+	for (let person of accepted) {
+		if (person.roles.includes('pilot') && 
+			!person.roles.includes('maintainer')) { 
+				pilotCount++ 
+		}
+		if (person.roles.includes('maintainer') &&
+			!person.roles.includes('pilot')) { 
+				maintCount++
+		}
+		if (person.roles.includes('pilot') && 
+			person.roles.includes('maintainer')) { 
+				pilotAndMaintCount++ 
+		}
+	}
+
+	if (pilotCount >= 2 && 
+		maintCount >= 1 && 
+		pilotCount + maintCount + pilotAndMaintCount >= 3) {
+		// TODO: Notifico il baseSup
+	}
+
+
+
+}
+
+module.exports = acceptMission
