bc008af41fb166aad12dc0bfc5154197ac9d0181
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2018-12-12 10:42:49 +0100

    Update schemas.js

--- db/schemas.js
+++ db/schemas.js
@@ -8,15 +8,12 @@
  * - Mission
  * - Logbook
  * - QTB
+ * - EventLog
 **/
 
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const ObjectId = mongoose.Schema.Types.ObjectId
-
-const eventTypes = ['comandoBot', 'notifica'] // TO DELETE
-//exports.scenarios = scenarios = ['a1', 'a2', 'a3', 'a4', 'b1', 'b2', 'c']
-//exports.riskLevel = riskLevel = {min: 1, max: 4}
 
 exports.droneTypes = droneTypes = ['VL', 'L', 'CRO']
 
