100bb453bb2e56147d4beb86a37630d043c8f7bc
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2018-12-13 13:34:35 +0100

    Update bot-functions.js

--- bot/bot-functions.js
+++ bot/bot-functions.js
@@ -1,26 +1,29 @@
 const queries   = require('../db/queries.js')
 const timers    = require('timers')
 const Personnel = queries.Personnel
+const Mission   = queries.Mission
 
 /**
  * Dizionario usato per de-comprimenre i nomi delle actions per evitare il limite di 64 byte
  * Nel caso si può aumentare la compressione dei nomi delle action
  */
 exports.unZip = unZip = {
-    orgMiss: 'organizeMission',
-    accMiss: 'acceptMission',
-    decMiss: 'declineMission',
-    aDrn2Msn: 'addDroneToMission'
+    orgMiss:    'organizeMission',
+    accMiss:    'acceptMission',
+    decMiss:    'declineMission',
+    aDrn2Msn:   'addDroneToMission',
+    shTem:      'showTeam'
 }
 /**
  * Dizionario usato per comprimenre i nomi delle actions per evitare il limite di 64 byte
  * Nel caso si può aumentare la compressione dei nomi delle action
  */
 exports.zip = zip = {
-    organizeMission: 'orgMiss',
-    acceptMission:   'accMiss',
-    declineMission:  'decMiss',
-    addDroneToMission: 'aDrn2Msn'
+    organizeMission:    'orgMiss',
+    acceptMission:      'accMiss',
+    declineMission:     'decMiss',
+    addDroneToMission:  'aDrn2Msn',
+    showTeam:           'shTem'
 }
 
 /**
@@ -36,7 +39,7 @@ const roleToOperation = {
 /**
  * Funzione che restituisce i comandi che una certa persione può eseguire in base ai ruoli che ricopre
  * 
- * @param {} aPerson 
+ * @param {Personnel} aPerson 
  */
 const getPermissions = aPerson => {
     var commands = [];
@@ -55,29 +58,21 @@ const getPermissions = aPerson => {
 /**
  * Funzione che recupera i dati di una persona dal db e li passa ad una funzione di callback
  */
-exports.loadData = (idTelegram, callback) => {
-    return new Promise((resolve, reject) => {
-        Personnel.findByIdTelegram(idTelegram, {})
-        .then(aPerson => {
-            resolve({
-                commands: getPermissions(aPerson),
-                person: aPerson
-            })
-        })
-        .catch(err => console.log(err))
-    })
+exports.loadData = async idTelegram => {
+    const aPerson = await Personnel.findByIdTelegram(idTelegram, '')
+    return { commands: getPermissions(aPerson), person: aPerson }
 }
 
 /**
  * Funzione che setta il bot come startato da una persona.
  * La persona ha già usato il comando /start.
  */
-exports.setBotStarted = idTelegram => queries.Personnel.updateByIdTelegram(idTelegram, {'telegramData.botStarted': true})
+exports.setBotStarted = idTelegram => Personnel.updateByIdTelegram(idTelegram, {'telegramData.botStarted': true})
 
 /**
  * Funzione che resetta il flag botStarted.
  */
-exports.resetBotStarted = idTelegram => queries.Personnel.updateByIdTelegram(idTelegram, {'telegramData.botStarted': false})
+exports.resetBotStarted = idTelegram => Personnel.updateByIdTelegram(idTelegram, {'telegramData.botStarted': false})
 
 /**
  * Funzione che controlla le missioni non istanziate ogni minuto e notifica l'AM dopo 15 minuti che non viene istanziata
@@ -85,7 +80,7 @@ exports.resetBotStarted = idTelegram => queries.Personnel.updateByIdTelegram(idT
 exports.checkTimeout = async () => {
     timers.setInterval(async () => {
         let now = new Date().getMinutes()
-        let missions = await queries.Mission.find({'status.requested.value': true, 'status.waitingForTeam.value': false}, 'status.requested.timestamp')
+        let missions = await Mission.find({'status.requested.value': true, 'status.waitingForTeam.value': false}, 'status.requested.timestamp')
         for (let m of missions) {
             let missionDate = new Date(m.status.requested.timestamp).getMinutes()
             if (now - missionDate == 1) { // DEBUG: cambiare in 15 minuti
