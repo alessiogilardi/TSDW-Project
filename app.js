42e5b5c197d2b26d47b10fa915ad218b1671aba5
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2019-02-07 21:49:21 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 require('dotenv').config()
-const db				= require('./db/db-connect.js')
-const Telegraf			= require('telegraf')
-const session			= require('telegraf/session')
-const dataLoader		= require('./bot/middlewares/data-loader')
-const bf				= require('./bot/bot-functions.js')
-const Stage				= require('telegraf/stage')
-const organizeMission	= require('./bot/scenes/organize-mission')
-const requestMission	= require('./bot/scenes/request-mission')
-const manageDrones		= require('./bot/scenes/manage-drones')
-const createTeam		= require('./bot/scenes/create-team')
-const addLogbook		= require('./bot/scenes/add-logbook')
-const addQtb			= require('./bot/scenes/add-qtb')
-const acceptMission		= require('./bot/actions/accept-mission')
-const eventRegister		= require('./events/event-register')
-const router			= require('./bot/router')
-const deadlineCheck		= require('./bot/missions-deadline-check')
-const { leave }			= Stage
+const db = require('./db/db-connect.js')
+const Telegraf = require('telegraf')
+const session = require('telegraf/session')
+const dataLoader = require('./bot/middlewares/data-loader')
+const bf = require('./bot/bot-functions.js')
+const Stage = require('telegraf/stage')
+const organizeMission = require('./bot/scenes/organize-mission')
+const requestMission = require('./bot/scenes/request-mission')
+const manageDrones = require('./bot/scenes/manage-drones')
+const createTeam = require('./bot/scenes/create-team')
+const addLogbook = require('./bot/scenes/add-logbook')
+const addQtb = require('./bot/scenes/add-qtb')
+const acceptMission = require('./bot/actions/accept-mission')
+const eventRegister = require('./events/event-register')
+const router = require('./bot/router')
+const deadlineCheck = require('./bot/missions-deadline-check')
+const { leave } = Stage
 
 
 const backtick = '\`';
 
- // TODO: definire una funzione che fa passare le missioni started a waitingForDocuments e setta il timestamp -> FATTO quando si inserisce il primo QTB
+// TODO: definire una funzione che fa passare le missioni started a waitingForDocuments e setta il timestamp -> FATTO quando si inserisce il primo QTB
 
 const stage = new Stage([organizeMission, requestMission, createTeam, manageDrones, addLogbook, addQtb])
 stage.command('cancel', leave())
@@ -29,19 +29,19 @@ const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN)
 eventRegister(bot)
 
 // session({ ttl: 10 })
-bot.use(session());
-bot.use(dataLoader());
+bot.use(session())
+bot.use(dataLoader())
 bot.use(stage.middleware())
 
 
 bot.start(ctx => {
-	if (ctx.session.userData.person.telegramData.botStarted)
-		return
+	if (ctx.session.userData.person.telegramData.botStarted) { return }
+
 	ctx.reply(`Ciao ${ctx.message.from.first_name}!`)
-	.then(() => ctx.reply(`Command list:\n ${ctx.session.userData.commands.join('\n')}`))
-	.catch(err => console.log(err));
+		.then(() => ctx.reply(`Command list:\n ${ctx.session.userData.commands.join('\n')}`))
+		.catch(err => console.log(err));
 	bf.setBotStarted(ctx.message.from.id)
-});
+})
 bot.help(ctx => ctx.reply(`Command list:\n${ctx.session.userData.commands.join('\n')}`));
 // TODO: action -> delete non funziona -> Da verificare il tipo di action
 // bot.action('delete', ctx => bf.resetBotStarted(ctx.message.from.id))
@@ -85,12 +85,12 @@ bot.command(['addQtb', 'addqtb'], ctx => {
 
 
 //////////// DEGUGGING DA CANCELLARE ////////////////////
-bot.hears(['A','a'], ctx => {
-    ctx.reply('DEBUG: creazione team', Telegraf.Extra
+bot.hears(['A', 'a'], ctx => {
+	ctx.reply('DEBUG: creazione team', Telegraf.Extra
 		.markdown()
-		.markup( m => m.inlineKeyboard([
+		.markup(m => m.inlineKeyboard([
 			m.callbackButton('Accetta', `${zip['createTeam']}:${'5c5c06bbd9397029782fef6b'}`)
-	])))
+		])))
 })
 /*
 bot.hears(['b', 'B'], ctx => {
@@ -121,13 +121,13 @@ router.on('declineMission', ctx => {
 })
 
 router.on('createTeam', ctx => {
-	ctx.scene.enter('createTeam', { mission: { _id: ctx.state.data[0] }})
+	ctx.scene.enter('createTeam', { mission: { _id: ctx.state.data[0] } })
 })
 
 bot.on('callback_query', router)
 
 db.connect(process.env.DB_ADDRESS, process.env.DB_PORT, process.env.DB_NAME)
-.then(() => {
-	console.log('Bot started!')
-	bot.startPolling()
-})
+	.then(() => {
+		console.log('Bot started!')
+		bot.startPolling()
+	})
