7b7e261d13427a70a603fb8279fe469cc7142384
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2019-02-07 21:21:36 +0100

    Update accept-mission.js

--- bot/actions/accept-mission.js
+++ bot/actions/accept-mission.js
@@ -1,9 +1,8 @@
-const queries   = require('../../db/queries')
-const bf 		= require('../bot-functions')
-const Personnel = queries.Personnel
-const Mission   = queries.Mission
-const zip 		= bf.zip
-const utils 	= require('../../utils')
+const utils 		 	= require('../../utils')
+const Telegraf 		 	= require('telegraf')
+const { zip, unZip } 	= require('../bot-functions')
+const { Personnel, Mission } = require('../../db/queries')
+
 
 /**
  * 1. Ricevo _id della missione accetttata tramite callbackButton
@@ -41,26 +40,28 @@ const notify = async (idTelegram, message, mission) => {
 /**
  * Genera il messaggio da inviare al baseSupervisor con il Personale che ha accettato la Missione
  * e i rispettivi ruoli possibili.
- * @param {Array} accepted 
+ * @param {Array}	accepted
+ * @param {Date}	missionDate
  */
 const generateMessage = async (accepted, missionDate) => {
 	let tmp = []
 	for (let person of accepted) {
 		let p = await Personnel.findById(person._id)
-		//p.roles = person.roles // Setto i ruoli che ha in questa missone
-		tmp.push(`${p.name}\t${p.surname}\t[${person.roles.join(', ')}]`)
+		tmp.push(`${p.name}\t${p.surname}\t->\t[${person.roles.join(', ')}]`)
 	}
-
 	return `C\'è un numero di persone sufficiente per formare un Team per la missione in data: ${utils.Date.format(missionDate, 'DD MMM YYYY')}\n\n${tmp.join('\n')}`
 }
 
-// Controlla che ci siano abbastanza persone per un team
+/**
+ * Funziona che controlla se c'è un numero sufficiente di persone per formare un team.
+ * @param {ObjectId} missionId 
+ */
 const checkForTeam = async missionId => {
 	const aMission = await Mission.findById(missionId, '')
 	const accepted = aMission.personnel.accepted
-	if (accepted.length < 3) {
-		return
-	}
+	
+	if (accepted.length < 3) { return }
+
 	// Se la missione dura meno di 3h
 	if (aMission.description.duration.expected < 3) {
 		let pilotCount = 0
@@ -68,8 +69,9 @@ const checkForTeam = async missionId => {
 			if (person.roles.includes('pilot')) 
 				pilotCount++
 		}
+
 		if (pilotCount >= 2) {
-			const message 	 = await generateMessage(accepted, mission.date)
+			const message 	 = await generateMessage(accepted, aMission.date)
 			const supervisor = await Personnel.findById(aMission.supervisor)
 			notify(supervisor.telegramData.idTelegram, message, aMission)
 		}
@@ -77,9 +79,8 @@ const checkForTeam = async missionId => {
 	}
 
 	// Se la missione dura più di 3h
-	if (accepted.length < 4) {
-		return
-	}
+	if (accepted.length < 4) { return }
+
 	let pilotCount = 0
 	let maintCount = 0
 	let pilotAndMaintCount = 0
@@ -105,7 +106,6 @@ const checkForTeam = async missionId => {
 		const supervisor = await Personnel.findById(aMission.supervisor)
 		notify(supervisor.telegramData.idTelegram, message, aMission)
 	}
-
 }
 
 const acceptMission = async (bot, ctx)=> {
@@ -115,17 +115,16 @@ const acceptMission = async (bot, ctx)=> {
 	const missionId = ctx.state.data[0]
 	let roles 	= ctx.state.data[1].split(',') // Ruoli che può ricoprire nella missione
 	for (let i in roles) {
-		roles[i] = bf.unZip[roles[i]]
+		roles[i] = unZip[roles[i]]
 	}
 	const person 	= ctx.session.userData.person
 	const aMission 	= await Mission.findById(missionId, '')
 
-
-	await Personnel.updateById(person._id, { $push: { 'missions.accepted': { idMission: aMission._id, date: aMission.date, roles: roles} } })
+	Personnel.updateById(person._id, { $push: { 'missions.accepted': { idMission: aMission._id, date: aMission.date, roles: roles} } })
 	// forse non serve rimuovere il personale dai notificati -> await Mission.updateById(aMission._id, { $pull: { 'personnel.notified': person._id } })
 	await Mission.updateById(aMission._id, { $push: { 'personnel.accepted': { _id: person._id, roles: roles} } })
 
-	checkForTeam(missionId)
+	checkForTeam(aMission._id)
 }
 
 module.exports = acceptMission
