5bcdcda8d2d5bb3c5fe8bb8ab83f81220e34fb0c
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2019-02-08 12:47:59 +0100

    Debug e code refactory

--- events/bot/on-team-created.js
+++ events/bot/on-team-created.js
@@ -1,3 +1,4 @@
+const Telegraf  = require('telegraf')
 const { Personnel, EventLog } = require('../../db/queries')
 const utils = require('../../utils')
 
@@ -5,7 +6,7 @@ const utils = require('../../utils')
  *  Event handler che gestisce le operazioni successive alla creazione di un Team
  * 
  *  1. Notifico chi è stato aggiunto alla missione
- *  2. Aggiungo la missione alle waitingForLogbook -> Implementato in funzione periodica: checkTodaysMissions
+ *  2. Aggiungo la missione alle waitingForLogbook -> Implementato in funzione periodica: checkTodayMissions
  *  3. Avviso chi non è stato scelto
  *  4. Rimuovo la missione dalle Accettate da chi non è stato scelto, in modo da
  *      renderlo nuovamente disponibile 
@@ -19,7 +20,7 @@ const onTeamCreated = async (bot, mission, team) => {
     const coPilotId    = team.pilots.co
     const crew         = team.crew
     const maintainers  = team.maintainers
-    const notified     = [chiefPilot, coPilot]
+    const notified     = [chiefPilotId, coPilotId]
 
     for (const p of crew) { notified.push(p) }
     for (const p of maintainers) { notified.push(p) }
@@ -29,9 +30,13 @@ const onTeamCreated = async (bot, mission, team) => {
         const chiefPilot  = await Personnel.findById(chiefPilotId)
         const coPilot     = await Personnel.findById(coPilotId)
         bot.telegram.sendMessage(chiefPilot.telegramData.idTelegram, 
-            `Sei stato aggiunto alla missione del giorno __${utils.Date.format(mission.date, 'DD MMM YYYY')}__ come __Chief Pilot__`)
+            `Sei stato aggiunto alla missione del giorno **${utils.Date.format(mission.date, 'DD MMM YYYY')}** come **Chief Pilot**`,
+            Telegraf.Extra.markdown())
+            .catch(err => {})
         bot.telegram.sendMessage(coPilot.telegramData.idTelegram, 
-            `Sei stato aggiunto alla missione del giorno __${utils.Date.format(mission.date, 'DD MMM YYYY')}__ come __Co Pilot__`)
+            `Sei stato aggiunto alla missione del giorno **${utils.Date.format(mission.date, 'DD MMM YYYY')}** come **Co Pilot**`,
+            Telegraf.Extra.markdown())
+            .catch(err => {})
     })()
 
     // Notifico la crew
@@ -40,7 +45,9 @@ const onTeamCreated = async (bot, mission, team) => {
             const person     = await Personnel.findById(p)
             const idTelegram = person.telegramData.idTelegram
             bot.telegram.sendMessage(idTelegram, 
-                `Sei stato aggiunto alla missione del giorno __${utils.Date.format(mission.date, 'DD MMM YYYY')}__ come __Crew__`)
+                `Sei stato aggiunto alla missione del giorno **${utils.Date.format(mission.date, 'DD MMM YYYY')}** come **Crew**`,
+                Telegraf.Extra.markdown())
+                .catch(err => {})
         }
     })()
 
@@ -50,18 +57,24 @@ const onTeamCreated = async (bot, mission, team) => {
             const person     = await Personnel.findById(p)
             const idTelegram = person.telegramData.idTelegram
             bot.telegram.sendMessage(idTelegram, 
-                `Sei stato aggiunto alla missione del giorno __${utils.Date.format(mission.date, 'DD MMM YYYY')}__ come __Maintainer__`)
+                `Sei stato aggiunto alla missione del giorno **${utils.Date.format(mission.date, 'DD MMM YYYY')}** come **Maintainer**`,
+                Telegraf.Extra.markdown())
+                .catch(err => {})
         }
     })()
 
     // Cerco tra quelli che avevano accettato e che non sono stati inseriti nel team
     ;(async () => {
-        for (let p of mission.personnel.accepted) {
-            if (!notified.includes(p._id)) { 
-                const person     = await Personnel.findById(p)
+        const accepted  = mission.personnel.accepted
+        const notif     = utils.arrayToArrayOfStrings(notified)
+        for (const p of accepted) {
+            if (!notif.includes(p._id.toString())) {
+                const person     = await Personnel.findById(p._id)
                 const idTelegram = person.telegramData.idTelegram
                 bot.telegram.sendMessage(idTelegram,
-                    `__NON__ sei stato aggiunto alla missione del giorno ${utils.Date.format(mission.date, 'DD MMM YYYY')}`)
+                    `**NON** sei stato aggiunto alla missione del giorno **${utils.Date.format(mission.date, 'DD MMM YYYY')}**`,
+                    Telegraf.Extra.markdown())
+                    .catch(err => {})
                 // Rimuovo la missione da quelle Accettate in modo che la persona sia di nuovo disponibile
                 Personnel.updateById(p, { $pull: { 'missions.accepted': { idMission: mission._id }}})
             }
