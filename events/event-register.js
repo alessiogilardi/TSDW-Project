7cc90376e13ff4ee47b8fbfa3e586c0a68f496f8
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2019-02-09 14:54:15 +0100

    Update event-register.js

--- events/event-register.js
+++ events/event-register.js
@@ -6,13 +6,15 @@ const ee                 = require('./event-emitters')
 const onRequestMission   = require('./bot/on-mission-requested')
 const onMissionOrganized = require('./bot/on-mission-organized')
 const onTeamCreated      = require('./bot/on-team-created')
+const onMissionAccepted  = require('./bot/on-mission-accepted')
 
 const register = bot => {
     if (bot === null || bot === undefined) throw new Error('Missing Telegram Bot')
 
-    ee.bot.on('missionRequested',   missions =>         onRequestMission(bot, missions))
-    ee.bot.on('missionOrganized',   mission  =>         onMissionOrganized(bot, mission))
-    ee.bot.on('teamCreated',        (mission, team) =>  onTeamCreated(bot, mission, team))
+    ee.bot.on('missionRequested',   missions            =>  onRequestMission(bot, missions))
+    ee.bot.on('missionOrganized',   mission             =>  onMissionOrganized(bot, mission))
+    ee.bot.on('missionAccepted',    (missionId, person) =>  onMissionAccepted(bot, missionId, person))
+    ee.bot.on('teamCreated',        (mission, team)     =>  onTeamCreated(bot, mission, team))
 }
 
 module.exports = register
