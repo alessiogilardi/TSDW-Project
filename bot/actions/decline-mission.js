ed2644711fd0b7709e2e4501c2d131c38870fe3d
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2019-02-16 14:06:40 +0100

    To be tested

--- bot/actions/decline-mission.js
+++ bot/actions/decline-mission.js
@@ -8,12 +8,21 @@ const parseParams = ctx => {
 	this.missionId 	= ctx.state.data[0]
 }
 
-const declineMission = async (ctx) => {
+/**
+ * Funzione che gestisce l'action declineMission.
+ * La persona che rifiuta la missione viene aggiunta a personnel.declined in Mission e
+ * inserisce l'evento nell'EventLog.
+ * 
+ * @param {Context} ctx
+ */
+const declineMission = async () => async (ctx) => {
     parseParams(ctx)
+    ctx.answerCbQuery('Missione rifiutata')
+    ctx.deleteMessage()
+    
     const person = ctx.person.userData
     Mission.updateById(this.missionId, { $push: { 'personnel.declined': { _id: person._id, timestamp: new Date() } }})
     EventLog.insert({ type: 'missionDeclined', actor: person._id, subject: {type: 'Mission', _id: this.missionId}, timestamp: new Date() })
-
 }
 
 module.exports = declineMission
