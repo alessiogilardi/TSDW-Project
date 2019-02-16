6f8d995abdc3290583c6e0a2cb5d29e83f203b48
Alessio Bollea <alessio.bollea@live.it>
2019-02-16 11:52:15 +0100

    Implementato checkGlobalTimeout (da testare)

--- bot/bot-functions.js
+++ bot/bot-functions.js
@@ -120,7 +120,7 @@ exports.checkOrganizeTimeout = async (bot) => {
             else
                 timeout = timeout_file.organize.long
             // Se il timeout è superato, si notifica l'AM
-            if (now - missionReq == timeout * 60000) { // il timeout è in minuti, quindi lo converto in msec per fare la sottrazione
+            if (now - missionReq >= timeout * 60000) { // il timeout è in minuti, quindi lo converto in msec per fare la sottrazione
                 // invio notifica all'AM
                 let am = await Personnel.find({'roles.command.airOperator.AM': true}, 'telegramData.idTelegram')
                 bot.sendMessage(am.telegramData.idTelegram, 'Una missione non è ancora stata accettata dal responsabile di base')
@@ -138,7 +138,33 @@ exports.checkGlobalTimeout = async (bot) => {
     timers.setInterval(async () => {
         // Carico il file con i timeout
         let timeout_file = JSON.parse(fs.readFileSync('timeouts.json', 'utf8'))
-        // TODO implementazione
+        // Controllo le missioni che ancora non hanno un team e per cui sono state contattate altre basi
+        let now = new Date().getTime()
+        let missions = await Mission.find({'status.waitingForTeam.value': true, 'status.teamCreated.value': false, 'status.aborted.value': false, notifiedBases: {$size: {$gt: 0}}}, '')
+        for (let m of missions) {
+            let missionReq  = new Date(m.status.requested.timestamp).getTime()
+            let missionDate = new Date(m.date).getTime()
+            let missionTeamReq  = new Date(m.status.waitingForTeam.timestamp).getTime()
+            // Se la differenza tra la data della missione e la data della richiesta è minore di 12 ore, usiamo il timeout breve
+            // altrimenti si usa il timeout lungo
+            let timeout = 0
+            if (missionDate - missionReq < 12 * 60 * 60 * 1000) // 12 ore * 60 min all'ora * 60 sec al min * 1000 msec al sec
+                timeout = timeout_file.organize.short
+            else
+                timeout = timeout_file.organize.long
+            // Se il timeout è superato, si notifica il base supervisor (distanza di tempo tra ora e quando sono state mandate le richieste)
+            if (now - missionTeamReq >= timeout * 60000) {
+                // invio messaggio al BS
+                let bs = await Personnel.findById(m.supervisor, 'telegramData.idTelegram')
+                let message = `La missione programmata per il giorno ${m.date} non ha ancora un team pronto.\n
+                               Vuoi annullare la missione?`
+                bot.telegram.sendMessage(bs.telegramData.idTelegram, message, Telegraf.Extra
+                    .markdown()
+                    .markup( m => m.inlineKeyboard([
+                        m.callbackButton('Abort', `${m._id}`)
+                ])))
+            }
+        }
     }, 1 * 60000)
 }
 
