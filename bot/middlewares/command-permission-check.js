f1940f6ce26948ad7cd03cbb7b5476bbdef349ce
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2019-02-17 13:11:26 +0100

    Update command-permission-check.js

--- bot/middlewares/command-permission-check.js
+++ bot/middlewares/command-permission-check.js
@@ -1,18 +1,50 @@
 const bf = require('../bot-functions')
 const utils = require('../../utils')
 
+const isText = (ctx) => {
+    return (ctx.updateType === 'message' && ctx.updateSubTypes.includes('text'))
+}
+
+const isCommand = (ctx) => {
+    return (isText(ctx) && ctx.update.message.text.startsWith('/'))
+}
+
+/**
+ * Funzione che restituisce i comandi disponibili all'utente.
+ * Comandi specifici del ruolo e comandi generici
+ * @param {Context} ctx 
+ */
+const getCommands = (ctx) => {
+    const commands  = utils.stringArray2LC(ctx.session.userData.commands)
+    const generic   = bf.genericCommands
+    return commands.concat(generic)
+}
+
+const hasRight = (ctx) => {
+    const text = ctx.update.message.text.toLowerCase()
+    return getCommands(ctx).includes(text)
+}
+
 const middleware = () => async (ctx, next) => {
-    if (ctx.updateType === 'message' && ctx.updateSubTypes.includes('text')) {
+    if (!isCommand(ctx) || hasRight(ctx)) {
+        return next() 
+    }
+    //if (!hasRight(ctx)) { return next() }
+    return ctx.reply('Non hai i diritti per eseguire questo comando!')
+
+    /*if (ctx.updateType === 'message' && ctx.updateSubTypes.includes('text')) {
         const text = ctx.update.message.text.toLowerCase()
         if (text.startsWith('/')) {
+        if (isCommand(ctx)) {
+            const text = ctx.update.message.text.toLowerCase()
             const commands = utils.arrayToLowerCase(ctx.session.userData.commands)
             if(commands.includes(text) || bf.genericCommands.includes(text)) {
                 return next()
             }
             return ctx.reply('Non hai i diritti per eseguire questo comando!')
         }
-    }
-    return next()
+    //}
+    return next()*/
 }
 
 module.exports = middleware
