5f1b9479673fc38a0c46e0eaff57bbffac85444b
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2019-02-18 23:03:32 +0100

    Riscritta checkForTeam

--- events/bot/on-mission-accepted.js
+++ events/bot/on-mission-accepted.js
@@ -1,7 +1,7 @@
 const { Personnel, Mission, EventLog } = require('../../db/queries')
 const utils = require('../../utils')
 const Telegraf = require('telegraf')
-const { zip, checkForTeam } = require('../../bot/bot-functions')
+const { zip } = require('../../bot/bot-functions')
 
 /**
  * Man mano che le persone accettano controllo quanti hanno
@@ -10,11 +10,13 @@ const { zip, checkForTeam } = require('../../bot/bot-functions')
  * - La missione dura meno di 3h:
  *      1. Ci sono almeno 3 persone
  *      2. Almeno 2 possono fare i piloti
+ *      3. Almeno 1 è crew
  *          --> Notifico il baseSup della missione per la scelta del Team
  * - La missione dura più di 3h
  *      1. Ci sono almeno 4 persone
  *      2. Almeno 2 possono fare i piloti
  *      3. Almeno 1 può fare il manutentore
+ *      4. Almeno 1 è crew
  * 		    --> Notifico il baseSup della missione per la scelta del Team
  * 
  * NOTE: il baseSup sceglierà i ruoli che ognuno avrà nella
@@ -54,82 +56,76 @@ const deletePrevMessage = async (chatId, messageId) => {
  */
 const generateMessage = async (accepted, missionDate) => {
 	let tmp = []
-	for (let person of accepted) {
-		let p = await Personnel.findById(person._id)
-		tmp.push(`${p.name}\t${p.surname}\t->\t\[${person.roles.join(', ')}\]`)
+	for (const person of accepted) {
+		const p = await Personnel.findById(person._id)
+		tmp.push(`${p.name}\t${p.surname}\t->\t[[${person.roles.join(', ')}]]`)
 	}
-	return `C\'è un numero di persone sufficiente per formare un Team per la missione in data: ${utils.Date.format(missionDate, 'DD MMM YYYY')}\n\n${tmp.join('\n')}`
+	return `C\'è un numero di persone sufficiente per formare un Team per la missione in data: ${utils.Date.format(missionDate, 'DD MMM YYYY kk:mm')}\n\n${tmp.join('\n')}`
 }
 
 /**
- * Funziona che controlla se c'è un numero sufficiente di persone per formare un team.
- * @param {ObjectId} missionId 
+ * Funzione che restituisce il numero di occorrenze di ogni ruolo
+ * per il personale che ha accettato la missione.
+ * @param {*} acceptedArray 
  */
-const checkForTeam = async aMission => {
-    const accepted = aMission.personnel.accepted
-    
-	if (accepted.length < 3) { return }
+const getRolesOccurrences = (acceptedArray) => {
+	let roles = acceptedArray.map(p => p.roles)
+		roles = utils.flatten(roles)
+		roles = utils.getOccurrences(roles)
 
+	return roles
+}
+
+/**
+ * Funzione che controlla se c'è un numero sufficiente di persone per compiere la Missione.
+ * Se la missione dura meno di 3h controlla che ci siano alemeno 2 piloti e 1 crew,
+ * altrimenti che ci siano almeno 2 pliloti, 1 crew e 1 manutentore.
+ * @param {Mission} aMission 
+ * @return {Boolean}
+ */
+const checkForTeam = (aMission) => {
+	const accepted = aMission.personnel.accepted
+
+	if (accepted.length < 3) { return false }
+
+	const roles = getRolesOccurrences(accepted)
+	
 	// Se la missione dura meno di 3h
 	if (aMission.description.duration.expected < 3) {
-		let pilotCount = 0
-		for (let person of accepted) {
-			if (person.roles.includes('pilot'))
-				pilotCount++
-		}
-
-		
-		if (pilotCount >= 2) {
-			const message = await generateMessage(accepted, aMission.date)
-            const supervisor = await Personnel.findById(aMission.supervisor)
-            if (aMission.status.waitingForTeam.createTeamButton.messageId) {
-                await deletePrevMessage(supervisor.telegramData.idTelegram, aMission.status.waitingForTeam.createTeamButton.messageId)
-            }
-            const { message_id } = await notify(supervisor.telegramData.idTelegram, message, aMission)
-            Mission.updateById(aMission._id, { $set: { 'status.waitingForTeam.createTeamButton.messageId': message_id }})
-		}
-		return
+		// Ritorno true se ci sono almeno 2 piloti e almeno 1 crew
+		return (roles['pilot'] >= 2 && roles['crew'] >= 1)
 	}
 
 	// Se la missione dura più di 3h
-	if (accepted.length < 4) { return }
+	if (accepted.length < 4) { return false }
 
-	let pilotCount = 0
-	let maintCount = 0
-	let pilotAndMaintCount = 0
-	for (let person of accepted) {
-		if (person.roles.includes('pilot') &&
-			!person.roles.includes('maintainer')) {
-			pilotCount++
-		}
-		if (person.roles.includes('maintainer') &&
-			!person.roles.includes('pilot')) {
-			maintCount++
-		}
-		if (person.roles.includes('pilot') &&
-			person.roles.includes('maintainer')) {
-			pilotAndMaintCount++
-		}
-	}
-
-	if (pilotCount >= 2 &&
-		maintCount >= 1 &&
-		pilotCount + maintCount + pilotAndMaintCount >= 3) {
-		const message = await generateMessage(accepted)
-		const supervisor = await Personnel.findById(aMission.supervisor)
-		const { message_id } = await notify(supervisor.telegramData.idTelegram, message, aMission)
-        Mission.updateById(aMission._id, { $set: { 'status.waitingForTeam.createTeamButton.messageId': message_id }})
-    }
-    
+	// Ritorno true se ci sono almeno 2 piloti, 1 crew e 1 manutentore
+	return (roles['pilot'] >= 2 && roles['crew'] >= 1 && roles['maintainer'] >= 1)
 }
 
+/**
+ * Ogniqualvolta una persona accetta una Missione controllo se è pronto un team e nel casonotifico il BS
+ * ed inserisco l'evento missionAccepted nell'EventLogbook.
+ * @param {*} bot 
+ * @param {*} aMissionId 
+ * @param {*} aPerson 
+ */
 const onMissionAccepted = async (bot, aMissionId, aPerson) => {
     if (bot === undefined || bot === null) throw new Error('Missing Telegram Bot')
     this.bot = bot
 
     const aMission 	= await Mission.findById(aMissionId, '')
-    
-    checkForTeam(aMission)
+    ;(async () => {
+		if (checkForTeam(aMission)) {
+			const message 		= await generateMessage(aMission.personnel.accepted, aMission.date)
+			const supervisor 	= await Personnel.findById(aMission.supervisor)
+			if (aMission.status.waitingForTeam.createTeamButton.messageId) {
+				await deletePrevMessage(supervisor.telegramData.idTelegram, aMission.status.waitingForTeam.createTeamButton.messageId)
+			}
+			const { message_id } = await notify(supervisor.telegramData.idTelegram, message, aMission)
+			await Mission.updateById(aMission._id, { $set: { 'status.waitingForTeam.createTeamButton.messageId': message_id }})
+		}
+	})()
     EventLog.insert({ type: 'missionAccepted', actor: aPerson._id, subject: {type: 'Mission', _id: aMission._id}, timestamp: new Date() })
 }
 
